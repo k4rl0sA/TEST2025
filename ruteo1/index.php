a6e0c070c8ae0f5163291a3aa875a1e349cee02d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-04 11:22:13 -0500

    06-04 Migracion 11:22

--- ruteo1/index.php
+++ ruteo1/index.php
@@ -10,7 +10,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/libs/nav.php';
 <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&family=Chicle&family=Merienda&family=Rancho&family=Boogaloo&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 <script src="../libs/js/a.js"></script>
-<script src="../libs/js/x.js?v=14.0"></script>
+<script src="../libs/js/x.js?v=15.0"></script>
 <script src="../libs/js/d.js"></script>
 <script src="../libs/js/popup.js"></script>
 <script>
