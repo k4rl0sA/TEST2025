4f08bf0f1f62acd16ea4b4fd02f4a1f4ece9dbcf
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-10 18:10:57 -0500

    06-10 Migracion 18:10

--- vsp/condsuic.php
+++ vsp/condsuic.php
@@ -326,7 +326,7 @@ function gra_condsuic(){
     'redu_riesgo_cierre', 'users_bina', 'equipo_bina', 'usu_creo', 'usu_update', 'fecha_update','estado'
   ];
   // Campos fecha que pueden ser nulos
-  $campos_fecha_null = ['fecha_obstetrica', 'fecha_cierre', 'fecha_update', 'fecha_create','peso_nacer'];
+  $campos_fecha_null = ['fecha_obstetrica', 'fecha_cierre', 'fecha_update','peso_nacer'];
 
   if(count($id)==4){
     // UPDATE
