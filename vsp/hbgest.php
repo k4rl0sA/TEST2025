bd540b9074b5f5f3890a6e1b7d7ef51eae17f610
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-10 17:29:39 -0500

    06-10 Migracion 17:29

--- vsp/hbgest.php
+++ vsp/hbgest.php
@@ -291,10 +291,10 @@ function gra_hbgest(){
     'estrategia_1', 'estrategia_2', 'acciones_1', 'desc_accion1', 'acciones_2', 'desc_accion2', 'acciones_3', 'desc_accion3',
     'activa_ruta', 'ruta', 'novedades', 'signos_covid', 'caso_afirmativo', 'otras_condiciones', 'observaciones',
     'cierre_caso', 'motivo_cierre', 'fecha_cierre', 'redu_riesgo_cierre', 'users_bina', 'equipo_bina',
-    'usu_creo', 'usu_update', 'fecha_update', 'fecha_create', 'estado'
+    'usu_creo', 'usu_update', 'fecha_update','estado'
   ];
   // Campos fecha que pueden ser nulos
-  $campos_fecha_null = ['fecha_obstetrica', 'fec_hb_recnac', 'fecha_cierre', 'fecha_update', 'fecha_create'];
+  $campos_fecha_null = ['fecha_obstetrica', 'fec_hb_recnac', 'fecha_cierre'];
 
   if(count($id)==4){
     // UPDATE
