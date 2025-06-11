2ad808d753bf689287c0efadcfb6172d5052d6b3
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-10 23:36:35 -0500

    06-10 Migracion 23:36

--- adm-dashboard/index.php
+++ adm-dashboard/index.php
@@ -124,29 +124,24 @@
             
             <section class="metrics-section">
 
-                <div class="metric-card pregnant">
-                    <div class="metric-icon">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M9 4c0-1.11.89-2 2-2s2 .89 2 2-.89 2-2 2-2-.89-2-2zm7 9c-.83 0-1.5-.67-1.5-1.5 0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5c0 .83-.67 1.5-1.5 1.5zm-3 7v-3h-2v3c0 .55-.45 1-1 1s-1-.45-1-1v-3c-1.1 0-2-.9-2-2v-3c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v3c0 1.1-.9 2-2 2v3c0 .55-.45 1-1 1s-1-.45-1-1z" fill="currentColor"/>
-                        </svg>
-                    </div>
-                    <div class="metric-content">
-                        <h3>Familias Con Actualización</h3>
-                        <div class="metric-value" id="familyUpdate">25.933</div>
-                        <div class="metric-subtitle"></div>
-                    </div>
-                </div>
-
                 <div class="metric-card families">
                     <div class="metric-icon">
-                        <!-- <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M16 4c0-1.11.89-2 2-2s2 .89 2 2-.89 2-2 2-2-.89-2-2zm4 18v-6h2.5l-2.54-7.63A1.5 1.5 0 0 0 18.54 8H17c-.8 0-1.54.37-2.01.99L12 14l-3-5.01A2.5 2.5 0 0 0 6.86 8H5.54c-.8 0-1.54.37-2.01.99L1 16h2.5v6h2v-6h2v6h2v-6h2v6h2z" fill="currentColor"/>
-                        </svg> -->
-                        
+                        <i class="fa-solid fa-house-user"></i>
                     </div>
                     <div class="metric-content">
                         <h3>Familias Caracterizadas</h3>
                         <div class="metric-value" id="totalFamilies">1.570.758</div>
+                        <div class="metric-subtitle"></div>
+                    </div>
+                </div>
+
+                <div class="metric-card pregnant">
+                    <div class="metric-icon">
+                        <i class="fa-solid fa-users"></i>
+                    </div>
+                    <div class="metric-content">
+                        <h3>Familias Con Actualización</h3>
+                        <div class="metric-value" id="familyUpdate">25.933</div>
                         <div class="metric-subtitle"></div>
                     </div>
                 </div>
