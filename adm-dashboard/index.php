e8f831120ae590574b9e4ecc749164e4e6f99575
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-10 23:17:10 -0500

    06-10 Migracion 23:17

--- adm-dashboard/index.php
+++ adm-dashboard/index.php
@@ -90,10 +90,7 @@
                 
                 <div class="metric-card consultations">
                     <div class="metric-icon">
-                        <i class="fa-solid fa-people-roof"></i>
-                        <!-- <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z" fill="currentColor"/>
-                        </svg> -->
+                        <i class="fa-solid fa-people-roof" style="font-size: 2.5em;"></i>
                     </div>
                     <div class="metric-content">
                         <h3>Familias Clasificadas</h3>
