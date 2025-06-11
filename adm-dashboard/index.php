da5ee89ac3c871105347abee361a7b12b0de84c5
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-10 22:42:09 -0500

    06-10 Migracion 22:42

--- adm-dashboard/index.php
+++ adm-dashboard/index.php
@@ -92,21 +92,6 @@
             <!-- Key Metrics -->
             <section class="metrics-section">
                 
-                
-
-                <div class="metric-card pregnant">
-                    <div class="metric-icon">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M9 4c0-1.11.89-2 2-2s2 .89 2 2-.89 2-2 2-2-.89-2-2zm7 9c-.83 0-1.5-.67-1.5-1.5 0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5c0 .83-.67 1.5-1.5 1.5zm-3 7v-3h-2v3c0 .55-.45 1-1 1s-1-.45-1-1v-3c-1.1 0-2-.9-2-2v-3c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v3c0 1.1-.9 2-2 2v3c0 .55-.45 1-1 1s-1-.45-1-1z" fill="currentColor"/>
-                        </svg>
-                    </div>
-                    <div class="metric-content">
-                        <h3>Familias Con Actualización</h3>
-                        <div class="metric-value" id="familyUpdate">25.933</div>
-                        <div class="metric-subtitle"></div>
-                    </div>
-                </div>
-
                 <div class="metric-card consultations">
                     <div class="metric-icon">
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -145,6 +130,19 @@
                         <h3>Familias Clasificadas</h3>
                         <div class="metric-value" id="totalFamilies">1.570.758</div>
                         <div class="metric-subtitle">Prioridades Bajas</div>
+                    </div>
+                </div>
+
+                <div class="metric-card pregnant">
+                    <div class="metric-icon">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M9 4c0-1.11.89-2 2-2s2 .89 2 2-.89 2-2 2-2-.89-2-2zm7 9c-.83 0-1.5-.67-1.5-1.5 0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5c0 .83-.67 1.5-1.5 1.5zm-3 7v-3h-2v3c0 .55-.45 1-1 1s-1-.45-1-1v-3c-1.1 0-2-.9-2-2v-3c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v3c0 1.1-.9 2-2 2v3c0 .55-.45 1-1 1s-1-.45-1-1z" fill="currentColor"/>
+                        </svg>
+                    </div>
+                    <div class="metric-content">
+                        <h3>Familias Con Actualización</h3>
+                        <div class="metric-value" id="familyUpdate">25.933</div>
+                        <div class="metric-subtitle"></div>
                     </div>
                 </div>
 
