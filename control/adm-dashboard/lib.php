1a56ea316b680717fe0a8b2d634798c1db5be95d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-12 13:20:08 -0500

    06-12 Migracion 13:20

--- control/adm-dashboard/lib.php
+++ control/adm-dashboard/lib.php
@@ -42,18 +42,18 @@ if ($caracterizaciones === 0) {
     exit;
 }
 
-/* $sql2= "SELECT COUNT(*) AS  FROM hog_fam hf LEFT JOIN hog_geo hg ON hf.idpre = hg.idgeo $where_sql;";
+$sql2= "SELECT COUNT(*) AS familia  FROM hog_fam hf LEFT JOIN hog_geo hg ON hf.idpre = hg.idgeo $where_sql;";
 $fam = datos_mysql($sql2);
 if ($fam['code'] !== 0 || empty($fam['responseResult'])) {
     echo json_encode(["error" => "Objeto no encontrado"]);
     exit;
 }
-$familias = $fam['responseResult'][0]['COUNT(*)'] ?? 0;
+$familias = $fam['responseResult'][0]['familia'] ?? 0;
 if ($familias === 0) {
     echo json_encode(["error" => "No se encontraron familias en el rango de fechas especificado."]);
     exit;
 }
- */
+
 
 
 // Simulación de datos, reemplaza por tus consultas reales
