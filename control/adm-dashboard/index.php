6c11201a122be9ce6fea8294cbd28af63f6cf144
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-14 20:50:28 -0500

    06-14 Migracion 20:50

--- control/adm-dashboard/index.php
+++ control/adm-dashboard/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard de Salud - EBEH</title>
-    <link rel="stylesheet" href="styles.css?v=12S.0">
+    <link rel="stylesheet" href="styles.css?v=13.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
