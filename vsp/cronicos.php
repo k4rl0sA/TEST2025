272b4e1febcb2b508a109903b7aa7cde63ee2f4d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-16 09:36:36 -0500

    06-16 Migracion 09:36

--- vsp/cronicos.php
+++ vsp/cronicos.php
@@ -258,10 +258,10 @@ function gra_cronicos(){
     'estrategia_1', 'estrategia_2', 'acciones_1', 'desc_accion1', 'acciones_2', 'desc_accion2', 'acciones_3', 'desc_accion3',
     'activa_ruta', 'ruta', 'novedades', 'signos_covid', 'caso_afirmativo', 'otras_condiciones', 'observaciones',
     'cierre_caso', 'motivo_cierre', 'fecha_cierre', 'redu_riesgo_cierre', 'users_bina', 'equipo_bina',
-    'usu_creo', 'usu_update', 'fecha_update', 'fecha_create', 'estado'
+    'usu_creo', 'usu_update', 'fecha_update', 'estado'
   ];
   // Campos fecha que pueden ser nulos
-  $campos_fecha_null = ['fecha_hemo', 'fecha_cierre', 'fecha_update', 'fecha_create'];
+  $campos_fecha_null = ['fecha_hemo', 'fecha_cierre', 'fecha_update','tas', 'tad', 'glucometria', 'peso', 'talla', 'imc', 'peri_cintura'];
 
   if(count($id)==4){
     // UPDATE
