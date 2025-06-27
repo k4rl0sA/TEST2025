889917946c6fce3b0c6895006ee8da57516a101f
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-06-27 16:32:43 -0500

    06-27 Migracion 16:32

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -532,8 +532,8 @@ if(!empty($_POST['fecha_gestion']) && !empty($_POST['usuario_gest'])){
 	array('type' => 'i', 'value' => $_SESSION['us_sds']),
 	array('type' => 's', 'value' => date("Y-m-d H:i:s"))
 	);
-	$rta3=show_sql($sql1, $params1);
-	// $rta1 = mysql_prepd($sql1, $params1);
+	// $rta3=show_sql($sql1, $params1);
+	$rta1 = mysql_prepd($sql1, $params1);
 	if (strpos($rta1, "correctamente") !== false){
 		$rta.= " Y Se ha asignado el predio";
 	}elseif(strpos($rta1, "Duplicate")){
