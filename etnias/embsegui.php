16e412a1c953e3edbeb3a8c00f5c344a228b12e5
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-02 09:49:50 -0500

    07-02 Migracion 09:49

--- etnias/embsegui.php
+++ etnias/embsegui.php
@@ -266,8 +266,9 @@ function formato_dato($a,$b,$c,$d){
     }
 		return $rta;
 }
-	function bgcolor($a,$c,$f='c'){
-		$rta="";
-		return $rta;
-	   }
+
+function bgcolor($a,$c,$f='c'){
+	$rta="";
+	return $rta;
+}
 	   
