c0d24ab7331ef3031f7b3b66913c755e72fd3b09
Jcurrea91 <ingsoportesds1@gmail.com>
2025-07-04 11:15:33 -0500

    07-04 Migracion 11:13

--- etnias/embsegnoreg.php
+++ etnias/embsegnoreg.php
@@ -94,7 +94,7 @@ function cmp_segnoreg(){
   $c[]=new cmp('fum','d',10,$d,$w.' '.$bl.' '.$ge.' '.$o,'Fecha Última Menstruación','fum',null,null,true,true,'','col-2',"validDate(this,-270,0);");
   $c[]=new cmp('edad_gest','s',3,$d,$w.' '.$bl.' '.$ge.' '.$o,'Edad Gestacional Al Momento De Identificacion En Semanas','edad_gest',null,null,true,true,'','col-4','enabEmbRGes();enabEmbPRg();');
   $c[]=new cmp('fecha_obs','d',10,$d,$w.' '.$bl.' Pue '.$o,'Fecha de Evento Obstetrico','fecha_obs',null,null,true,false,'','col-2');
-  $c[]=new cmp('resul_gest','s',3,$d,$w.' '.$bl.' Pue '.$o,'Resultado De La Gestación','resul_gest',null,null,true,false,'','col-2',"enabNV();");
+  $c[]=new cmp('resul_gest','s',3,$d,$w.' '.$bl.' Pue '.$o,'Resultado De La Gestación','resul_gest',null,null,false,false,'','col-2',"enabNV();");
   $c[]=new cmp('peso_nacer','sd',5,$d,$w.' '.$bl.' Nav '.$o,'Peso Al Nacer (Gr)','peso_nacer',null,null,false,false,'','col-2');
   $c[]=new cmp('asist_controles','s',3,$d,$w.' PrG '.$bl.' '.$o,'¿Asiste A Controles Prenatales?','rta',null,null,true,true,'','col-2');
   $c[]=new cmp('exa_labo','s',3,$d,$w.' PrG '.$bl.' '.$o,'¿Cuenta Con Exámenes De Laboratorio Al Día? Con Relación Al Trimestre Gestacional?','rta',null,null,true,true,'','col-4');
