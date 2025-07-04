880d6fb61400408e22b2ca01f127f6c9cb0a06d3
Jcurrea91 <ingsoportesds1@gmail.com>
2025-07-04 15:01:59 -0500

    07-04 Migracion 15:01

--- libs/js/x.js
+++ libs/js/x.js
@@ -1429,7 +1429,8 @@ function disFall(){
 }
 function etnFall(){
 	const conditions = [
-		{ id: 'estado_seg', value: '1', compare: true }
+		{ id: 'estado_seg', value: '1', compare: true },
+		{ id: 'prioridad', value: '1', compare: true }
 	];
 	EnabDepeDynamic(['signosA'], conditions);
 }
