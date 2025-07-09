95b78abc257b2c274c507e270a113289a36e4a7d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-09 13:08:52 -0500

    07-09 FixTableros 13:08

--- control/adm-dashboard/lib.php
+++ control/adm-dashboard/lib.php
@@ -126,7 +126,8 @@ AND P.estado='A' AND P.fecha_nacimiento IS NOT NULL
 GROUP BY Rango_Edad ORDER BY Rango_Edad;";
 $age = datos_mysql($sql4);
 if ($age['code'] !== 0 || empty($age['responseResult'])) {
-    echo json_encode(["error" => "No se encontraron datos para distribución por edad, por favor valide los filtros."]);//.var_dump($sql4)
+    echo json_encode(["error" => "No se encontraron datos para distribución por edad, por favor valide los filtros."]);
+    var_dump($sql4)
     exit;
 }
 $age_distribution = [];
