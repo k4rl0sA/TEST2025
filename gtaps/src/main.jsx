2712155a98a09c225a327fc13f5c7be8d5f92685
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-12 10:41:46 -0500

    07-12 GTAPS v2 10:41

--- gtaps/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-const root= ReactDOM.createRoot(document.getElementById('root'))
-root.render("Hola Mundo");
