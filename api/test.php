c0d6de9f8a19b330c7c2add1bba49776658f1940
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-12 23:34:07 -0500

    07-12 GTAPS v2 23:34

--- api/test.php
+++ api/test.php
@@ -12,5 +12,5 @@ echo json_encode([
     'get' => $_GET,
     'post' => array_intersect_key($_POST, array_flip(['key1', 'key2'])), // Replace 'key1', 'key2' with required keys
     'hasheada' => password_verify('Hogar2020+', '$2y$10$QNrQsbSBb1i5kh8xcLR9.O8I3cBF/gnlZdQXh8112TXf/J0hI0xOC'),
-    'hasheada' =>password_verify('SFRRFRDfdfcsdswew654', $clave_en_bd);
+    'claveOk' => password_verify('Hogar2020+', '$2y$10$QNrQsbSBb1i5kh8xcLR9.O8I3cBF/gnlZdQXh8112TXf/J0hI0xOC');
 ]);
