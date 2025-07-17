dad042acbd08dfabcc3f58bc1560bc7577b37ee2
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-17 17:07:49 -0500

    07-17 RiesgoFam 17:07

--- adm-gestusu/lib.php
+++ adm-gestusu/lib.php
@@ -177,7 +177,7 @@ function opc_perfil($id=''){
     if (adm()=='ADM') {
         return opc_sql("SELECT idcatadeta, descripcion FROM `catadeta` WHERE idcatalogo = 218 AND estado = 'A'",$id);
     } else {
-        $com = datos_mysql("SELECT CASE WHEN componente = 'EAC' THEN 2 WHEN componente = 'HOG' THEN 1 END as componente FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}'");
+        $com = datos_mysql("SELECT CASE WHEN componente = 'MIN' THEN 2 WHEN componente = 'HOG' THEN 1 END as componente FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}'");
         $comp = $com['responseResult'][0]['componente'] ;
         return opc_sql("SELECT idcatadeta, descripcion FROM `catadeta` WHERE idcatalogo = 218 AND estado = 'A' AND valor='$comp'",$id);
     }
