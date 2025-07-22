6b0784926c99980a30ff18f72706e95bac9e9430
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-22 01:04:52 -0500

    07-22 ValidaUser 01:04

--- crea-fam/index.php
+++ crea-fam/index.php
@@ -195,7 +195,7 @@ function grabar(tb='',ev){
             let rta = d;
             try { rta = JSON.parse(d); } catch(e){}
             if(rta && rta.success){
-              ok(rta.msg+' Por favor espere para que se actualice la información.');
+              ok(rta.msg+' Por favor espere para que se actualice la información desde SDS.');
               // ok(rta.msg);
               // alert(rta.msg + " Estado: " + rta.estado);
             } else {
@@ -206,7 +206,7 @@ function grabar(tb='',ev){
           warnin("Operación cancelada por el usuario.");
         }
       } else if(resp && resp.success){
-        ok(resp.msg);
+        ok(resp.msg+' Por favor de click en Mostrar Integrantespara actualizar ');
         // alert(resp.msg + " Estado: " + resp.estado);
       } else {
         // Si no es JSON, mostrar como antes
