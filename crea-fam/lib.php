6ce6959377e877d7b8fa0a64443abf4379f01578
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-22 23:30:31 -0500

    07-22 traslados 23:30

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -1021,6 +1021,8 @@ function formato_dato($a,$b,$c,$d){
 				$rta .= acceso('uaic') ? "<li title='Seguimientos UAIC' onclick=\"mostrar('uaic_seg','pro',event,'','../etnias/uaicseg.php',7,'Seguimientos');Color('datos-lis');\"><i class=' fa-solid fa-list-ol ico' id='{$c['ACCIONES']}'></i></li>":"";
 			}
 			$rta .= acceso('servagen') ? "<li title='Servicio de Agendamiento' onclick=\"mostrar('servagen','pro',event,'','../agendamient/serage.php',7,'Servicio de Agendamiento');Color('datos-lis');\"><i class=' fa-solid fa-square-phone ico' id='{$c['ACCIONES']}'></i></li>":"";
+
+			$rta.="<li title='Trasladar Usuario' Onclick=\"mostrar('traslados','pro',event,'','trasladloc.php',7,'traslados');Color('datos-lis');\"><i class='fas fa-exchange-alt ico' id='".$c['ACCIONES']."'></i> </li>";
 		}
 	}
 		if($a=='atencion' && $b=='acciones'){
