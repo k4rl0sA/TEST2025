664eed63d2a1aa5182c7b9ea954ead650190351e
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-24 15:32:41 -0500

    07-24 Agendas 15:32

--- agendas/index.php
+++ agendas/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Modular con Filtros, Tabla y Formularios</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link href="../libs/css/s.css" rel="stylesheet">
+    <link href="../libs/css/app.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
