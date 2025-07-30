11b273b8f136717eb9f4f97bc08baa4b9b49a065
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-07-30 15:43:11 -0500

    07-30 Agendas 15:43

--- adm-gestusu/lib.php
+++ adm-gestusu/lib.php
@@ -166,7 +166,7 @@ function opc_perfilusuario($id=''){
             perfil=(select descripcion from catadeta c where idcatalogo=218 and idcatadeta='{$_REQUEST['id']}' and estado='A') 
             and componente=(SELECT componente FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}') 
             and subred=(SELECT subred FROM usuarios WHERE id_usuario ='{$_SESSION['us_sds']}') ORDER BY nombre";
-            //echo $sql;
+            echo $sql;
             $info = datos_mysql($sql);		
             return json_encode($info['responseResult']);	
         }
