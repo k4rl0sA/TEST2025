13b2bd65faebd9514858c492fe18a1ffc0a33113
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-04 10:50:39 -0500

    08-04 Agendas 10:50

--- agendas/lib.php
+++ agendas/lib.php
@@ -91,8 +91,9 @@ if ($req == 'getAppointments') {
     $professionalId = intval($_GET['professionalId'] ?? 0);
     $weekStart = $_GET['weekStart'] ?? '';
     $weekEnd = $_GET['weekEnd'] ?? '';
-    $sql = "SELECT idagenda, cupo, profesionalid, idpeople, idgeo, fecha, actividad, notas, estado
-            FROM agendas
+    $sql = "SELECT A.idagenda, A.cupo, A.profesionalid, A.idpeople, A.idgeo, A.fecha, A.actividad, A.notas, A.estado,P.tipo_doc,P.idpersona
+            FROM agendas A
+            LEFT JOIN person P ON 
             WHERE profesionalid=$professionalId AND fecha BETWEEN '$weekStart' AND '$weekEnd' ";
     $result = datos_mysql($sql);
     $appointments = [];
