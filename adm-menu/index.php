998c8824583bdf9e96c5a8b551eb7447e6dbd746
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-20 01:15:55 -0500

    08-20 Planillas  01:15

--- adm-menu/index.php
+++ adm-menu/index.php
@@ -12,11 +12,11 @@ if (empty($_SESSION['csrf_token'])) {
     <title>Gestión de Menú</title>
     <link rel="stylesheet" href="../lib/css/app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
-    <link rel="stylesheet" href="../lib/css/choices.min.css?v=3">
+    <link rel="stylesheet" href="../lib/css/choices.min.css?v=4">
     <script src="../lib/js/choices.min.js"></script>
     <script>window.CSRF_TOKEN = "<?php echo $_SESSION['csrf_token']; ?>";</script>
-    <script src="../lib/js/app.js?v=3" defer></script>
-    <script src="menu.js?v=3"></script>
+    <script src="../lib/js/app.js?v=4" defer></script>
+    <script src="menu.js?v=4"></script>
 </head>
 <body>
     <div class="toast-container">
