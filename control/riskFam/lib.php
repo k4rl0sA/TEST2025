2cafe69a2448713302f2cecd17cee2e47fe68407
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-20 14:48:46 -0500

    08-20 Planilla 14:48

--- control/riskFam/lib.php
+++ control/riskFam/lib.php
@@ -29,10 +29,7 @@ $datos = $res['responseResult'][0];
 
 //Riesgo Socioeconómico
 $sql1="SELECT G.estrato AS 'Estrato',FN_CATALOGODESC(13,C.ingreso) AS 'Ingreso', ROUND(((CASE G.estrato  WHEN 1 THEN 6 WHEN 2 THEN 5 WHEN 3 THEN 4 WHEN 4 THEN 3 WHEN 5 THEN 2 WHEN 6 THEN 1 ELSE 0 END + CASE C.ingreso  WHEN 1 THEN 3 WHEN 2 THEN 2  WHEN 3 THEN 1 ELSE 0 END ) - 2) * 100 / 7,2) AS SE
-FROM `person` P 
-LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam
-LEFT JOIN hog_geo G ON F.idpre = G.idgeo
-LEFT JOIN hog_carac C ON F.id_fam = C.idfam
+FROM `person` P LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam LEFT JOIN hog_geo G ON F.idpre = G.idgeo LEFT JOIN hog_carac C ON F.id_fam = C.idfam
 WHERE C.fecha = (SELECT MAX(C2.fecha) FROM hog_carac C2 WHERE C2.idfam = C.idfam) 
 AND P.idpersona = '$document'  AND P.tipo_doc='$tipo' LIMIT 1";
 
@@ -47,10 +44,7 @@ $ingreso= $res1['responseResult'][0]['Ingreso'];
 
 //Riesgo Estructura Familiar
 $sql2="SELECT A.descripcion AS 'Descripcion',ROUND(((CASE A.descripcion WHEN 'DISFUNCIÓN FAMILIAR SEVERA' THEN 4 WHEN 'DISFUNCIÓN FAMILIAR MODERADA' THEN 3  WHEN 'DISFUNCIÓN FAMILIAR LEVE' THEN 2  WHEN 'FUNCIÓN FAMILIAR NORMAL' THEN 1 ELSE 0 END - 1) * 100 / 3), 2) AS EF_porcentaje
-FROM `person` P 
-LEFT JOIN hog_tam_apgar A ON P.idpeople = A.idpeople
-LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam
-LEFT JOIN hog_geo G ON F.idpre = G.idgeo
+FROM `person` P LEFT JOIN hog_tam_apgar A ON P.idpeople = A.idpeople LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam LEFT JOIN hog_geo G ON F.idpre = G.idgeo
 WHERE A.fecha_toma = (SELECT MAX(A2.fecha_toma) FROM hog_tam_apgar A2  WHERE A2.idpeople = A.idpeople) AND A.descripcion IS NOT NULL 
 AND P.idpersona = '$document'  AND P.tipo_doc='$tipo' LIMIT 1;";
 $res2 = datos_mysql($sql2);
@@ -63,80 +57,28 @@ $estruFamil = $res2['responseResult'][0]['EF_porcentaje'];
 $apgar = $res2['responseResult'][0]['Descripcion'];
 
 //Riesgo Vulnerabilidad Social
-$sql3="SELECT  
-  P.idpeople,-- Seguridad alimentaria (cada ítem por separado)
+$sql3="SELECT  P.idpeople,-- Seguridad alimentaria (cada ítem por separado)
   CASE WHEN C.seg_pre1 = 'SI' THEN 5 ELSE 0 END AS seg_pre1_puntaje,CASE WHEN C.seg_pre2 = 'SI' THEN 5 ELSE 0 END AS seg_pre2_puntaje,CASE WHEN C.seg_pre3 = 'SI' THEN 5 ELSE 0 END AS seg_pre3_puntaje, CASE WHEN C.seg_pre4 = 'SI' THEN 5 ELSE 0 END AS seg_pre4_puntaje,  CASE WHEN C.seg_pre5 = 'SI' THEN 5 ELSE 0 END AS seg_pre5_puntaje,  CASE WHEN C.seg_pre6 = 'SI' THEN 5 ELSE 0 END AS seg_pre6_puntaje,  CASE WHEN C.seg_pre7 = 'SI' THEN 5 ELSE 0 END AS seg_pre7_puntaje,  CASE WHEN C.seg_pre8 = 'SI' THEN 5 ELSE 0 END AS seg_pre8_puntaje, -- Puntaje por población diferencial
-  CASE P.pobladifer   WHEN 1 THEN 5 WHEN 2 THEN 5 WHEN 3 THEN 5 WHEN 4 THEN 4 
-    WHEN 5 THEN 5 WHEN 10 THEN 3 WHEN 11 THEN 4 WHEN 13 THEN 5   ELSE 0 END AS puntaje_pobladifer,  -- Puntaje por inclusión por oficio
-  CASE P.incluofici WHEN 1 THEN 5 WHEN 3 THEN 4 WHEN 4 THEN 3 WHEN 5 THEN 5
-    WHEN 6 THEN 3 WHEN 7 THEN 4 WHEN 8 THEN 5  ELSE 0 END AS puntaje_incluofici, -- Puntaje bruto total vulnerabilidad social
-  (CASE WHEN C.seg_pre1 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre2 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre3 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre4 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre5 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre6 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre7 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre8 = 'SI' THEN 5 ELSE 0 END +
-    CASE P.pobladifer   WHEN 1 THEN 5 WHEN 2 THEN 5 WHEN 3 THEN 5 WHEN 4 THEN 4
-      WHEN 5 THEN 5 WHEN 10 THEN 3 WHEN 11 THEN 4 WHEN 13 THEN 5    ELSE 0 END +
-    CASE P.incluofici  WHEN 1 THEN 5 WHEN 3 THEN 4 WHEN 4 THEN 3 WHEN 5 THEN 5  WHEN 6 THEN 3 WHEN 7 THEN 4 WHEN 8 THEN 5   ELSE 0 END
-  ) AS puntaje_total_vulnerabilidad_social, -- Porcentaje escala 0 a 100 (máximo 50)
-  ROUND(((CASE WHEN C.seg_pre1 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre2 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre3 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre4 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre5 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre6 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre7 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre8 = 'SI' THEN 5 ELSE 0 END +
-      CASE P.pobladifer    WHEN 1 THEN 5 WHEN 2 THEN 5 WHEN 3 THEN 5 WHEN 4 THEN 4
-      WHEN 5 THEN 5 WHEN 10 THEN 3 WHEN 11 THEN 4 WHEN 13 THEN 5  ELSE 0 END +
-      CASE P.incluofici   WHEN 1 THEN 5 WHEN 3 THEN 4 WHEN 4 THEN 3 WHEN 5 THEN 5
-        WHEN 6 THEN 3 WHEN 7 THEN 4 WHEN 8 THEN 5 ELSE 0 END
-    ) * 100.0 / 50), 2) AS vulnerabilidad_social_porcentaje
-FROM person P
-LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam
-LEFT JOIN hog_carac C ON F.id_fam = C.idfam
-WHERE C.fecha = (  SELECT MAX(C2.fecha)
-  FROM hog_carac C2
-  WHERE C2.idfam = C.idfam
-) AND P.idpersona = '$document' AND P.tipo_doc = '$tipo' LIMIT 1";
+  CASE P.pobladifer   WHEN 1 THEN 5 WHEN 2 THEN 5 WHEN 3 THEN 5 WHEN 4 THEN 4 WHEN 5 THEN 5 WHEN 10 THEN 3 WHEN 11 THEN 4 WHEN 13 THEN 5   ELSE 0 END AS puntaje_pobladifer,  -- Puntaje por inclusión por oficio
+  CASE P.incluofici WHEN 1 THEN 5 WHEN 3 THEN 4 WHEN 4 THEN 3 WHEN 5 THEN 5 WHEN 6 THEN 3 WHEN 7 THEN 4 WHEN 8 THEN 5  ELSE 0 END AS puntaje_incluofici, -- Puntaje bruto total vulnerabilidad social
+  (CASE WHEN C.seg_pre1 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre2 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre3 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre4 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre5 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre6 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre7 = 'SI' THEN 5 ELSE 0 END +   CASE WHEN C.seg_pre8 = 'SI' THEN 5 ELSE 0 END +   CASE P.pobladifer   WHEN 1 THEN 5 WHEN 2 THEN 5 WHEN 3 THEN 5 WHEN 4 THEN 4 WHEN 5 THEN 5 WHEN 10 THEN 3 WHEN 11 THEN 4 WHEN 13 THEN 5 ELSE 0 END + CASE P.incluofici  WHEN 1 THEN 5 WHEN 3 THEN 4 WHEN 4 THEN 3 WHEN 5 THEN 5  WHEN 6 THEN 3 WHEN 7 THEN 4 WHEN 8 THEN 5 ELSE 0 END) AS puntaje_total_vulnerabilidad_social, -- Porcentaje escala 0 a 100 (máximo 50)
+  ROUND(((CASE WHEN C.seg_pre1 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre2 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre3 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre4 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre5 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre6 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre7 = 'SI' THEN 5 ELSE 0 END +  CASE WHEN C.seg_pre8 = 'SI' THEN 5 ELSE 0 END + CASE P.pobladifer    WHEN 1 THEN 5 WHEN 2 THEN 5 WHEN 3 THEN 5 WHEN 4 THEN 4  WHEN 5 THEN 5 WHEN 10 THEN 3 WHEN 11 THEN 4 WHEN 13 THEN 5  ELSE 0 END + CASE P.incluofici   WHEN 1 THEN 5 WHEN 3 THEN 4 WHEN 4 THEN 3 WHEN 5 THEN 5  WHEN 6 THEN 3 WHEN 7 THEN 4 WHEN 8 THEN 5 ELSE 0 END) * 100.0 / 50), 2) AS vulnerabilidad_social_porcentaje  
+FROM person P LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam LEFT JOIN hog_carac C ON F.id_fam = C.idfam 
+WHERE C.fecha = (SELECT MAX(C2.fecha) FROM hog_carac C2  WHERE C2.idfam = C.idfam) 
+AND P.idpersona = '$document' AND P.tipo_doc = '$tipo' LIMIT 1";
 $res3 = datos_mysql($sql3);
 $vulnSocial = $res3['responseResult'][0]['vulnerabilidad_social_porcentaje'];
 $puntajeTotal = $res3['responseResult'][0]['puntaje_total_vulnerabilidad_social'];
 
 //Riesgo Acceso a Servicios de Salud
-$sql4="SELECT 
-    P.idpeople,
-    -- Puntaje según el régimen
-    CASE P.regimen
-        WHEN 1 THEN 4
-        WHEN 2 THEN 2
-        WHEN 3 THEN 1
-        WHEN 4 THEN 5
-        WHEN 5 THEN 6
-        ELSE 0
-    END AS puntaje_regimen_salud,
-    -- Porcentaje sobre 6 puntos
-    ROUND(
-        CASE P.regimen
-            WHEN 1 THEN 4
-            WHEN 2 THEN 2
-            WHEN 3 THEN 1
-            WHEN 4 THEN 5
-            WHEN 5 THEN 6
-            ELSE 0
-        END * 100 / 6, 2
-    ) AS acceso_salud_porcentaje,
-    -- Ponderación al 10%
-    ROUND(
-        CASE P.regimen
-            WHEN 1 THEN 4
-            WHEN 2 THEN 2
-            WHEN 3 THEN 1
-            WHEN 4 THEN 5
-            WHEN 5 THEN 6
-            ELSE 0
-        END * 10 / 6, 8
-    ) AS acceso_salud_ponderado
-FROM person P
-LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam
-LEFT JOIN hog_geo G ON F.idpre = G.idgeo
-LEFT JOIN hog_carac C ON F.id_fam = C.idfam
-WHERE C.fecha = (
-    SELECT MAX(C2.fecha)
-    FROM hog_carac C2
-    WHERE C2.idfam = C.idfam
-) AND P.idpersona = '$document' AND P.tipo_doc = '$tipo' LIMIT 1";
+$sql4="SELECT P.idpeople,-- Puntaje según el régimen
+    CASE P.regimen WHEN 1 THEN 4    WHEN 2 THEN 2   WHEN 3 THEN 1 WHEN 4 THEN 5 WHEN 5 THEN 6 ELSE 0 END AS puntaje_regimen_salud,  -- Porcentaje sobre 6 puntos
+    ROUND( CASE P.regimen  WHEN 1 THEN 4 WHEN 2 THEN 2 WHEN 3 THEN 1 WHEN 4 THEN 5 WHEN 5 THEN 6 ELSE 0  END * 100 / 6, 2) AS acceso_salud_porcentaje, -- Ponderación al 10%
+    ROUND(CASE P.regimen  WHEN 1 THEN 4  WHEN 2 THEN 2 WHEN 3 THEN 1 WHEN 4 THEN 5  WHEN 5 THEN 6 ELSE 0  END * 10 / 6, 8) AS acceso_salud_ponderado
+FROM person P LEFT JOIN hog_fam F ON P.vivipersona = F.id_fam LEFT JOIN hog_geo G ON F.idpre = G.idgeo LEFT JOIN hog_carac C ON F.id_fam = C.idfam
+WHERE C.fecha = (SELECT MAX(C2.fecha) FROM hog_carac C2 WHERE C2.idfam = C.idfam) AND 
+P.idpersona = '$document' AND P.tipo_doc = '$tipo' LIMIT 1";
 $res4 = datos_mysql($sql4);
-
 if ($res4['code'] !== 0 || empty($res4['responseResult'])) {
     echo json_encode(["error" => "Datos de acceso a servicios de salud no encontrados", "document" => $document]);
     exit;
@@ -146,12 +88,8 @@ $puntajeRegimenSalud = $res4['responseResult'][0]['puntaje_regimen_salud'];
 
 //Riesgo Entorno Habitacional
 $sql5="SELECT P.idpersona,  -- Zona y su puntaje
-  FN_CATALOGODESC(3,G.zona) AS Zona,
-  CASE WHEN G.zona = 1 THEN 1
-       WHEN G.zona = 2 THEN 2 ELSE NULL END AS Puntaje_Zona,
-  -- Tipo de vivienda y puntaje
-  FN_CATALOGODESC(4,C.tipo_vivienda) AS 'Tipo de Vivienda',
-  CASE C.tipo_vivienda
+  FN_CATALOGODESC(3,G.zona) AS Zona, CASE WHEN G.zona = 1 THEN 1 WHEN G.zona = 2 THEN 2 ELSE NULL END AS Puntaje_Zona, -- Tipo de vivienda y puntaje
+  FN_CATALOGODESC(4,C.tipo_vivienda) AS 'Tipo de Vivienda',  CASE C.tipo_vivienda
     WHEN 1 THEN 1 
     WHEN 2 THEN 1 
     WHEN 3 THEN 2
