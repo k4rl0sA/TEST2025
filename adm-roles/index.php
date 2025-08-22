52d23446c6324d8e17f6f80679745d28c71a249e
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-22 01:36:14 -0500

    08-22 Planillas  01:36

--- adm-roles/index.php
+++ adm-roles/index.php
@@ -19,13 +19,13 @@ if (!isset($_SESSION["us_sds"])) {
     <meta name="keywords" content="roles, gestión, administración, permisos, sistema, filtros, seguridad">
     <meta name="author" content="Carlos Eduardo Acevedo Arevalo">
     <link rel="canonical" href="https://tusitio.com/adm-ajustes/index.html">
-    <link rel="stylesheet" href="../lib/css/app.css?v=5">
+    <link rel="stylesheet" href="../lib/css/app.css?v=6">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
-    <link rel="stylesheet" href="../lib/css/choices.min.css?v=5">
+    <link rel="stylesheet" href="../lib/css/choices.min.css?v=6">
     <script src="../lib/js/choices.min.js"></script>
     <script>window.CSRF_TOKEN = "<?php echo $_SESSION['csrf_token']; ?>";</script>
-    <script src="../lib/js/app.js?v=5" defer></script>
-    <script src="ajustes.js?v=5"></script>
+    <script src="../lib/js/app.js?v=6" defer></script>
+    <script src="ajustes.js?v=6"></script>
 </head>
 <body>
     <div class="toast-container">
