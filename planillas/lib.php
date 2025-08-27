4c080fa1fcbb89145f1fc448049de9d37b396596
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-27 16:08:43 -0500

    08-27 Planillas 16:08

--- planillas/lib.php
+++ planillas/lib.php
@@ -94,7 +94,7 @@ function cmp_planillas(){
 	$c[]= new cmp('tipo_doc','s','3',$d['tipo_doc']??'',$w.' '.$key.' '.$o,'Tipo documento','tipo_doc',null,null,true,$edit,'','col-3',"getDatForm('pEr','personOld',['infgen'],this);");
     $c[]= new cmp('nombre_completo','t',50,$d['nombre_completo']?? '',$w.' '.$o, 'Nombre Completo','','','',false,false,'','col-5');
     $c[]= new cmp('perfil','s',3,'',$w.' '.$o,'Perfil','perfil',null,'',true,true,'','col-2',"changeSelect('perfil','colaborador');");//  ,"enabDepeValu('perfil','uSR');
-    $c[]= new cmp('colaborador','s',20,$d['colaborador'] ?? '',$w.' uSR '.$o, 'Colaborador','','','',false,true,'','col-5');
+    $c[]= new cmp('colaborador','s',20,$d['colaborador'] ?? '',$w.' uSR '.$o, 'Colaborador','colaborador','','',false,true,'','col-5');
     $c[]= new cmp('estado_planilla','s',3,$d['estado_planilla'] ?? '',$w.' '.$o, 'Estado Planilla', 'estado_planilla','','',true,true,'','col-4');
     $c[]= new cmp('carpeta','t',50,$d['carpeta'] ?? '',$w.' '.$o, 'Carpeta','','','',true,true,'','col-6');
     $c[]= new cmp('caja','t',50,$d['caja'] ?? '',$w.' '.$o, 'Caja','','','',true,true,'','col-6');
