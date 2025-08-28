de0e75137ea278a28c606186ca413f0fb673d69c
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-28 15:22:44 -0500

    08-28 Planillas 15:22

--- planillas/lib.php
+++ planillas/lib.php
@@ -125,7 +125,7 @@ function get_personOld(){
    	WHERE idpersona ='".$id[0]."' AND tipo_doc='".$id[1]."'";
 	$info=datos_mysql($sql);
 	if (!$info['responseResult']) {
-        return $rta="Error: El usuario con este número de documento NO se encuentra registrado.";
+        return json_encode (new stdClass);
 	}else{
 		return json_encode($info['responseResult'][0]);
 	}
