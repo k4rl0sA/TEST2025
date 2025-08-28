488d3a7e557a0245fdbab9f850b254af0a93a5be
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-28 15:18:55 -0500

    08-28 Planillas 15:18

--- planillas/lib.php
+++ planillas/lib.php
@@ -125,7 +125,7 @@ function get_personOld(){
    	WHERE idpersona ='".$id[0]."' AND tipo_doc='".$id[1]."'";
 	$info=datos_mysql($sql);
 	if (!$info['responseResult']) {
-        echo json_encode(["error" => "No se encontraron caracterizaciones en el rango de fechas especificado."]);
+        return $rta="Error: El usuario con este número de documento NO se encuentra registrado.";
 	}else{
 		return json_encode($info['responseResult'][0]);
 	}
