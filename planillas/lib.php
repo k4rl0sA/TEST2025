3c2bd9e3814ee9ee5c82197784b9efd1c6c4bc87
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-29 10:40:07 -0500

    08-28 Planillas 10:40

--- planillas/lib.php
+++ planillas/lib.php
@@ -69,6 +69,7 @@ function cap_menus($a,$b='cap',$con='con') {
   if ($a=='planillas'  && isset($acc['crear']) && $acc['crear']=='SI'){  
     $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
     $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
+    $rta .= "<li class='icono $a ajustar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
   }
   return $rta;
 }
