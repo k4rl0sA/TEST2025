82119c42eb9dc9e70ce1506e4088a1d92985aa21
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-08-29 12:05:43 -0500

    08-28 Planillas 12:05

--- planillas/lib.php
+++ planillas/lib.php
@@ -157,7 +157,7 @@ FUNCTION lis_family(){
   // CAMBIO P.tipo_doc,P.idpersona
 	$sql="SELECT CASE WHEN EXISTS (
         SELECT 1 FROM hog_carac C
-        WHERE C.idfam = 2554 AND C.fecha = '2025-08-02' AND C.usu_create = 123456789 AND C.estado = 'A'
+        WHERE C.idfam = 1 AND C.fecha = '2024-10-08' AND C.usu_create = 80811594 AND C.estado = 'A'
     ) THEN '✔' ELSE '✘' END AS Caracterización";
 	
     /* $sql.=" WHERE A.estado = 'A' AND A.idpeople='".$id[0]; // CAMBIO  AGREGAR ESTA LINEA 
