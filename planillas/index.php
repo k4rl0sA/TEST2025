ecd16da4f7635a1e11b421d473433fd449104f30
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-01 11:12:24 -0500

    09-01 Planillas 11:12

--- planillas/index.php
+++ planillas/index.php
@@ -46,7 +46,7 @@ async function cargarResumenFamiliar() {
   		let html = "<table style='width:100%; border-collapse:collapse;'><tr><th>Validación</th><th>Estado</th><th>Fecha</th></tr>";
   		data.forEach(row => {
   		  let icono = row.estado === 'Completado' ? "<span style='color:green;'>✔</span>" : "<span style='color:red;'>✘</span>";
-  		  html += `<tr><td>${row.nombre} ${row.fecha_ultima}</td><td>${icono} </td><td>${row.fecha_ultima}</td></tr>`;
+  		  html += `<tr><td>${row.nombre}</td><td>${icono} </td><td>${row.fecha_ultima}</td></tr>`;
   		});
   		html += "</table>";
   		document.getElementById('valida-family').innerHTML = html;
