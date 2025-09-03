0f82e5afdbda0e0b14ab9514507beb09a8488f5f
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-03 12:06:30 -0500

    09-03 Planillas 12:06

--- planillas/index.php
+++ planillas/index.php
@@ -47,7 +47,7 @@ async function cargarResumenFamiliar() {
   		let html = "<table style='width:100%; border-collapse:collapse;'><tr><th>Validación</th><th>Estado</th><th>Fecha</th><th>Ajustar</th></tr>";
   		data.forEach(row => {
   		  let icono = row.estado === 'Completado' ? "<span style='color:green;'>✔</span>" : "<span style='color:red;'>✘</span>";
-		  let ajustar = (row.fecha_ultima && row.estado === 'Validar') ? `<i class='fa-solid fa-screwdriver-wrench icon' style='cursor:pointer;color:mediumblue;text-shadow: 3px 3px 1px rgb(255 210 0);font-size:18px;' title='Ajustar' id='${row.id || ''}' onclick=\"mostrarCmp('${row.id || ''}','${row.nombre}')\"></i>` : '';// && 
+		  let ajustar = (row.fecha_ultima && row.estado === 'Validar') ? `<i class='fa-solid fa-screwdriver-wrench icon' style='cursor:pointer;color:mediumblue;text-shadow: 3px 3px 1px rgb(255 210 0);font-size:18px;' title='Ajustar' id='${row.id || ''}' onclick=\"mostrar('person','pro',event,'','lib.php',7,'person');\"></i>` : '';// && 
 		  html += `<tr><td>${row.nombre}</td><td>${icono}</td><td>${row.fecha_ultima || ''}</td><td style='text-align:center;'>${ajustar}</td></tr>`;
   		});
   		html += "</table>";
