45eb0e1ea97df2db9c075fa681093655a51fabb7
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-08 09:55:25 -0500

    09-08 FixEscalaRiesgo 09:55

--- control/riskFam/index.php
+++ control/riskFam/index.php
@@ -468,7 +468,11 @@ session_start();
       if (key === 'individualConditions') {
         factors[key].puntaje = riskFactors[key].puntaje || 0;
         factors[key].gestante = riskFactors[key].gestante || '';
-        factors[key].ges_sinctrl = riskFactors[key].ges_sinctrl || '';
+        factors[key].["Gestante Sin Control"]= riskFactors[key].ges_sinctrl || '';
+        factors[key].cronico = riskFactors[key].cronico || '';
+        factors[key].["Cronico Sin Control"] = riskFactors[key].cro_sinctrl || '';
+        factors[key].["Menor De 5 Años"] = riskFactors[key].menor5 || '';
+        
         
       }
         /*// Actualizar puntaje de entorno habitacional si existe
