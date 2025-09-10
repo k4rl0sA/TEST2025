cd3fc8e73197e1042886db5012b85ecb02f24d66
Jcurrea91 <ingsoportesds1@gmail.com>
2025-09-10 14:58:51 -0500

    09-10 Valries 14:58

--- valor-ries/lib.php
+++ valor-ries/lib.php
@@ -93,10 +93,9 @@ function cmp_tamvalories(){
 	$c[]=new cmp('valories_nombre','t','50',$d['valories_nombre'],$w.' '.$o,'nombres','valories_nombre',null,'',false,false,'','col-4');
 	$c[]=new cmp('valories_fechanacimiento','d','10',$d['valories_fechanacimiento'],$w.' '.$o,'fecha nacimiento','valories_fechanacimiento',null,'',false,false,'','col-15');
     $c[]=new cmp('valories_edad','n','3',$d['valories_edad'],$w.' '.$o,'edad','valories_edad',null,'',true,false,'','col-1');
-	$c[]=new cmp('tipoaccion','s',3,'',$w.' '.$o,'Tipo de Accion','tipoaccion',null,null,true,true,'','col-10');
 	$c[]=new cmp('fecha_toma','d','10','',$w.' '.$o,'fecha de la Toma','fecha_toma',null,'',true,true,'','col-2',"validDate(this,$days,0);");
+	$c[]=new cmp('tipoaccion','s',3,'',$w.' '.$o,'Tipo de Accion','tipoaccion',null,null,true,true,'','col-10');
 	
-    
 	$o='comprencion';
 	$c[]=new cmp($o,'e',null,'1. Comprensión y comunicación ',$w);
 	$c[]=new cmp('comprension1','s',3,'',$w.' '.$o,'Concentrarse en algo durante 10 minutos','nivel',null,null,true,true,'','col-10');
