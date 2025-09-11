cc5de3fdbf989d3bb56c87b4ca6eeef1b3735b32
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-11 13:16:25 -0500

    09-11 TamCarlos 13:16

--- carlos-crafft/lib.php
+++ carlos-crafft/lib.php
@@ -52,64 +52,60 @@ function lis_tamcarlos(){
 function lis_valories(){
 	$id=divide($_POST['id']);
 	$sql="SELECT id_valories 'Cod Registro',momento,porcentaje_total,analisis,`nombre` Creó,`fecha_create` 'fecha Creó'
-	FROM hog_tam_valories A
-	LEFT JOIN  usuarios U ON A.usu_creo=U.id_usuario ";
-	$sql.="WHERE idpeople='".$id[0];
-	$sql.="' ORDER BY fecha_create";
-	// echo $sql;
-	$datos=datos_mysql($sql);
-	return panel_content($datos["responseResult"],"valories-lis",5);
-}
+	function gra_tamcarlosen() {
+		// Validar que se recibe la edad por POST
+		if (!isset($_POST['edad']) || !is_numeric($_POST['edad'])) {
+			return "Edad no proporcionada o inválida.";
+		}
+		$edad = intval($_POST['edad']);
+		$idpeople = isset($_POST['idpeople']) ? intval($_POST['idpeople']) : 0;
+		if ($idpeople <= 0) {
+			return "ID de persona no válido.";
+		}
+		if ($edad < 16 && empty($_POST['fecha_toma'])) {
+			return "La fecha de toma es obligatoria para menores de 16 años.";
+		}
 
-function whe_tamcarlos() {
-	$sql = '1';
-    if (!empty($_POST['fidentificacion'])) {
-        $sql .= " AND P.idpersona = '".$_POST['fidentificacion']."'";
-    }
-    if (!empty($_POST['ffam'])) {
-        $sql .= " AND V.id_fam = '".$_POST['ffam']."'";
-    }
-    return $sql;
-}
+		// Preguntas 2 a 8
+		$campos = [
+			'sustancias', 'condualcoh', 'dismalcoh', 'estadoanimo', 'lios', 'olvido', 'solo'
+		];
+		$total = 0;
+		foreach ($campos as $campo) {
+			$val = isset($_POST[$campo]) ? intval($_POST[$campo]) : 0;
+			$total += $val;
+		}
 
-function cmp_tamcarlos(){
-	$rta="<div class='encabezado valories'>TABLA valories</div><div class='contenido' id='valories-lis'>".lis_tamcarlos()."</div></div>";
-	$t=['tam_valories'=>'','valories_tipodoc'=>'','valories_nombre'=>'','valories_idpersona'=>'','valories_fechanacimiento'=>'','valories_puntaje'=>'','valories_momento'=>'','valories_edad'=>'','valories_lugarnacimiento'=>'','valories_condicionsalud'=>'','valories_estadocivil'=>'','valories_escolaridad'=>'',
-	 'valories_ocupacion'=>'','valories_rutina'=>'','valories_rol'=>'',	 'valories_actividad'=>'','valories_evento'=>'','valories_comportamiento'=>'','porcentaje_comprension'=>'','porcentaje_moverse'=>'','porcentaje_cuidado'=>'','porcentaje_relacionarce'=>'','porcentaje_actividades'=>'','porcentaje_participacion'=>'','porcentaje_total'=>'','valories_analisis'=>''];
+		// Clasificación
+		if ($total <= 2) {
+			$descripcion = 'Consumo funcional / No consumo';
+		} else {
+			$descripcion = 'Consumo disfuncional (mayor riesgo)';
+		}
 
-	$w='tamcarlos';
-	$d=get_tamcarlos(); 
-	if ($d=="") {$d=$t;}
-	$o='datos';
-    $key='srch';
-	$days=fechas_app('psicologia');
-	$c[]=new cmp($o,'e',null,'DATOS DE IDENTIFICACIÓN',$w);
-	$c[]=new cmp('id','h',15,$_POST['id'],$w.' '.$o,'','',null,'####',false,false);
-	$c[]=new cmp('valories_idpersona','t','20',$d['valories_idpersona'],$w.' '.$o.' '.$key,'N° Identificación','valories_idpersona',null,'',false,false,'','col-2');
-	$c[]=new cmp('valories_tipodoc','s','3',$d['valories_tipodoc'],$w.' '.$o.' '.$key,'Tipo Identificación','tipodoc',null,'',false,false,'','col-25','getDatForm(\'srch\',\'person\',[\'datos\']);');
-	$c[]=new cmp('valories_nombre','t','50',$d['valories_nombre'],$w.' '.$o,'nombres','valories_nombre',null,'',false,false,'','col-4');
-	$c[]=new cmp('valories_fechanacimiento','d','10',$d['valories_fechanacimiento'],$w.' '.$o,'fecha nacimiento','valories_fechanacimiento',null,'',false,false,'','col-15');
-    $c[]=new cmp('edad','n','3',$d['valories_edad'],$w.' '.$o,'edad','valories_edad',null,'',true,false,'','col-1');
-	$c[]=new cmp('fecha_toma','d','10','',$w.' '.$o,'fecha de la Toma','fecha_toma',null,'',true,true,'','col-2',"validDate(this,$days,0);");
-	
-	$o='Tamizaje';
-	$c[]=new cmp($o,'e',null,'Preguntas',$w);
-	$c[]=new cmp('bebidas','s',3,'',$w.' '.$o,'¿Ha consumido bebidas alcohólicas (más de unos pocos sorbos)?','rta',null,null,true,true,'','col-10');
-	$c[]=new cmp('sustancias','s',3,'',$w.' '.$o,'¿Ha usado algún otro tipo de sustancias que alteren su estado de ánimo o de conciencia?','rta',null,null,true,true,'','col-10');
-	$c[]=new cmp('condualcoh','s',3,'',$w.' '.$o,'¿Alguna vez ha salido a la calle, o se ha subido a un carro conducido por  alguien (que podía ser usted mismo), estando bajo los efectos de alcohol?','rta',null,null,true,true,'','col-10');
-	$c[]=new cmp('dismalcoh','s',3,'',$w.' '.$o,'¿Alguna vez su familia o sus amigos le han dicho que debería disminuir el consumo de alcohol o de','rta',null,null,true,true,'','col-10');
-	$c[]=new cmp('estadoanimo','s',3,'',$w.' '.$o,'¿Alguna vez ha usado alcohol o drogas para reconfortarse (para sentirse mejor, para socializar, para mejorar su estado de ánimo, para','rta',null,null,true,true,'','col-10');
-	$c[]=new cmp('lios','s',3,'',$w.' '.$o,'¿Alguna vez ha tenido líos o problemas (peleas físicas o verbales; suspensiones académicas; detención por la policía; accidentes) estando bajo los efectos de alcohol o de drogas?','rta',null,null,true,true,'','col-10');
-	$c[]=new cmp('olvido','s',3,'',$w.' '.$o,'¿Alguna vez ha olvidado cosas que hizo estando bajo los efectos de alcohol o de drogas?','rta',null,null,true,true,'','col-10');
-	$c[]=new cmp('solo','s',3,'',$w.' '.$o,'¿Alguna vez ha consumido alcohol o drogas estando solo?','rta',null,null,true,true,'','col-10');
-    
-    $o='totalresul';
-	$c[]=new cmp($o,'e',null,'Resultado',$w);
-    $c[]=new cmp('total','t',3,'',$w.' '.$o,'Puntaje','total',null,'',false,false,'','col-4');
-    $c[]=new cmp('descripcion','t','3','',$w.' '.$o,'Descripcion','descripcion',null,null,false,false,'','col-5');
-	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
-	return $rta;
-}
+		// Preparar consulta y parámetros
+		$sql = "INSERT INTO tam_carlos_crafft (
+			idpeople, fecha_toma, bebidas, sustancias, condualcoh, dismalcoh, estadoanimo, lios, olvido, solo, total, descripcion, usu_creo, fecha_create, estado
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?)";
+		$params = [
+			['type' => 'i', 'value' => $idpeople],
+			['type' => 's', 'value' => $_POST['fecha_toma']],
+			['type' => 's', 'value' => $_POST['bebidas']],
+			['type' => 's', 'value' => $_POST['sustancias']],
+			['type' => 's', 'value' => $_POST['condualcoh']],
+			['type' => 's', 'value' => $_POST['dismalcoh']],
+			['type' => 's', 'value' => $_POST['estadoanimo']],
+			['type' => 's', 'value' => $_POST['lios']],
+			['type' => 's', 'value' => $_POST['olvido']],
+			['type' => 's', 'value' => $_POST['solo']],
+			['type' => 'i', 'value' => $total],
+			['type' => 's', 'value' => $descripcion],
+			['type' => 's', 'value' => $_SESSION['us_sds']],
+			['type' => 's', 'value' => 'A']
+		];
+		$rta = mysql_prepd($sql, $params);
+		return $rta;
+	}
 
 	function get_tamcarlos(){
 		if($_POST['id']==0){
@@ -146,7 +142,7 @@ function men_tamcarlos(){
 	return $rta;
   }
    
-function gra_tamcarlosen() {
+function gra_tamcarlos() {
 	// Validar que se recibe la edad por POST
 	if (!isset($_POST['edad']) || !is_numeric($_POST['edad'])) {
 		return "Edad no proporcionada o inválida.";
