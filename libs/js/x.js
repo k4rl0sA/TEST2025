4e4e92514318b4c8904cff4d27f05997a614005b
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-22 16:51:40 -0500

    09-22 Laborat 16:51

--- libs/js/x.js
+++ libs/js/x.js
@@ -1579,7 +1579,7 @@ function enabGestionLab(){
 }
 function enabTomaLab(){
 	const conditions = [
-		{ id: 'lab_tomado', value: '1', compare: true }
+		{ id: 'lab_tomado', value: '2', compare: false }
 	];
 	EnabDepeDynamic(['TaL'], conditions);
 }
