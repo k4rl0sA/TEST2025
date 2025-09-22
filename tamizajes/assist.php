da8994c9d1266794b6dac44d54f6e5e8268da56c
Jcurrea91 <ingsoportesds1@gmail.com>
2025-09-22 15:55:56 -0500

    09-22 TamAssist 15:55

--- tamizajes/assist.php
+++ tamizajes/assist.php
@@ -357,21 +357,7 @@ function gra_tamassist() {
     $sql = "INSERT INTO tam_assist (idpeople, fecha_toma, tconsumido, tfrecuencia, tdeseo, tsalud, thabitual, tpreocupa, tcontrolar, bconsumido, bfrecuencia, bdeseo, bsalud, bhabitual, bpreocupa, bcontrolar, cconsumido, cfrecuencia, cdeseo, csalud, chabitual, cpreocupa, ccontrolar,
         coconsumido, cofrecuencia, codeseo, cosalud, cohabitual, copreocupa, cocontrolar, aconsumido, afrecuencia, adeseo, asalud, ahabitual, apreocupa, acontrolar, iconsumido, ifrecuencia, ideseo, isalud, ihabitual, ipreocupa, icontrolar, trconsumido, trfrecuencia, trdeseo, trsalud, trhabitual, trpreocupa, trcontrolar,
         alconsumido, alfrecuencia, aldeseo, alsalud, alhabitual, alpreocupa, alcontrolar, oconsumido, ofrecuencia, odeseo, osalud, ohabitual, opreocupa, ocontrolar, otconsumido, otfrecuencia, otdeseo, otsalud, othabitual, otpreocupa, otcontrolar, inyec, pts_tabaco, nvl_tabaco, pts_bebida, nvl_bebida,pts_cannabis, nvl_cannabis,pts_cocaina, nvl_cocaina,pts_anfetaminas, nvl_anfetaminas,pts_inhalantes, nvl_inhalantes,pts_tranqui, nvl_tranqui,pts_alucinog, nvl_alucinog,pts_opiaceos, nvl_opiaceos,pts_otros, nvl_otros,nvl_inyec,usu_creo, fecha_create,estado)
-        VALUES (
-        ?, ?, ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, NOW(), ?)";
-    /* var_dump($_POST['fecha_toma']);*/
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?)";
     $params = [ 
         ['type' => 'i', 'value' => $idpeople],
         ['type' => 's', 'value' => $_POST['fecha_toma']],
