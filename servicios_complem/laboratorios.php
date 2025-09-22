060bf457e4a4b12823e9385a8a1463542f33a7d8
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-22 13:00:27 -0500

    09-22 Medicam 13:00

--- servicios_complem/laboratorios.php
+++ servicios_complem/laboratorios.php
@@ -198,7 +198,7 @@ function opc_tipo_lab($id=''){
   return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=23 and estado='A' ORDER BY 1",$id);
 }
 function opc_lab_tomado($id=''){
-  return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=24 and estado='A' ORDER BY 1",$id);
+  return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=170 and estado='A' ORDER BY 1",$id);
 }
 function opc_cuenta_resul($id=''){
   return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=25 and estado='A' ORDER BY 1",$id);
