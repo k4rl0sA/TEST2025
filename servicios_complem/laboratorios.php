b705f0a64ca76eda3483fa160f0f4955a8045a79
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-24 11:57:36 -0500

    09-23 Laborat 11:57

--- servicios_complem/laboratorios.php
+++ servicios_complem/laboratorios.php
@@ -234,9 +234,9 @@ function formato_dato($a,$b,$c,$d){
     // var_dump($d);
     $rta=$c[$d];
     if ($a=='laboratorios' && $b=='cod laboratorio'){
-        $rta="<nav class='menu right'>";    
+        $rta="<nav class='menu right'>";
         $rta.="<li class='icono editar' title='Editar' id='".$c['Cod Laboratorio']."' Onclick=\"setTimeout(getData,500,'laboratorios',event,this,['cod_admision','tipo_lab','otro_lab','fecha_orden','fecha_toma','cuenta_resul','fecha_resul','dato_crit','gestion','gest_cump'],'../servicios_complem/laboratorios.php');setTimeout(funNew,500,'../servicios_complem/laboratorios.php');\"></li>";
-        $rta.=['gestante']=='SI'?"<li title='Resultado Laboratorios' Onclick=\"mostrar('resultados','pro',event,'','../servicios_complem/laboratorios.php',4,'resultLab');Color('datos-lis');\"><i class='fa-solid fa-file-waweform ico' id='".$c['Cod Laboratorio']."'></i> </li>":'';
+        $rta.=['tomado']=='SI'?"<li title='Resultado Laboratorios' Onclick=\"mostrar('resultados','pro',event,'','../servicios_complem/laboratorios.php',4,'resultLab');Color('datos-lis');\"><i class='fa-solid fa-file-waweform ico' id='".$c['Cod Laboratorio']."'></i> </li>":'';
         $rta.="</nav>";
     }
     return $rta;
