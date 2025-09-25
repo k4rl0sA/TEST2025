c8afa9bd9eeda0c2e96da1f7e01bbab5e9770bf3
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-25 16:40:55 -0500

    09-25 Laborat 16:40

--- servicios_complem/laboratorios.php
+++ servicios_complem/laboratorios.php
@@ -292,9 +292,10 @@ FROM hog_laboratorios
  /* if ($_POST['cuenta_resul'] == '1' && (empty($_POST['fecha_resul']) || !validateDate($_POST['fecha_resul']))) {
       return "msj['Error: La fecha de resultado es obligatoria y debe ser una fecha válida cuando se cuenta con resultado.']";
     }
+  */
     if ($_POST['dato_crit'] == '1' && empty($_POST['gestion'] && empty($_POST['gest_cump'])) {
       return "msj['Error: La gestión es obligatoria cuando hay dato crítico. Por favor ingrese la gestión y la fecha de gestión.']";
-    }  */
+    }  
      $sql = "UPDATE hog_laboratorios SET cuenta_resul=?, fecha_resul=?, dato_crit=?, gestion=?, gest_cump=?, usu_update= ?, fecha_update=DATE_SUB(NOW(), INTERVAL 5 HOUR) WHERE id_lab=?";
         $params = [
             ['type' => 's', 'value' => trim($_POST['cuenta_resul'] ??null )],
