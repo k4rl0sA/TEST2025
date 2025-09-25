1825f0d80237c02f147c7838dd99ff549e5c9b7f
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-25 14:55:17 -0500

    09-25 Laborat 14:55

--- servicios_complem/laboratorios.php
+++ servicios_complem/laboratorios.php
@@ -226,7 +226,7 @@ function opc_cod_admision($id=''){
 }
 
 
- function cmp_resultLab(){
+/*  function cmp_resultLab(){
   $rta="";
   $t=['id_lab'=>'','cuenta_resul'=>'','fecha_resul'=>'','dato_crit'=>'','gestion'=>'','gest_cump'=>'','obs'=>''];
   $d=get_respuesta();
@@ -266,6 +266,106 @@ function men_resultLab(){
   $rta=cap_menus('resultLab','pro');
   return $rta;
 }
+ */
+
+
+
+
+
+
+function focus_resultLab(){
+	return 'resultLab';
+   }
+   
+   
+   function men_resultLab(){
+	$rta=cap_menus('resultLab','pro');
+	return $rta;
+   }
+   
+   function cap_menus($a,$b='cap',$con='con') {
+	 $rta = ""; 
+	 $acc=rol($a);
+	   if ($a=='resultLab'  && isset($acc['crear']) && $acc['crear']=='SI'){  
+	 $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>"; //~ openModal();
+	   }
+  return $rta;
+}
+
+function cmp_resultLab(){
+  $rta ="";
+    $w="placuifam";
+      $o='accide';
+      $e="";
+      $key='pln';
+      $o='resultLab';
+    //   var_dump($_POST);
+      $t=['compromiso'=>''];
+	$d=get_compromiso();
+	if ($d==""){$d=$t;}
+	$days=fechas_app('vivienda');
+      $c[]=new cmp($o,'e',null,'PLAN DE CUIDADO FAMILIAR CONCERTADO',$w);
+      $c[]=new cmp('idcom','h',15,$_POST['id'],$w.' '.$key.' '.$o,'id','id',null,'####',false,false);
+      $c[]=new cmp('compromiso','a',50,$d['compromiso'],$w.''.$o,'Compromisos concertados','observaciones',null,null,true,false,'','col-0');
+      $c[]=new cmp('fechac','d','3',$e,$w.' '.$o,'Fecha','fecha',null,null,true,true,'','col-5',"validDate(this,$days,0);");
+      $c[]=new cmp('tipo','s','2',$e,$w.' '.$o,'Tipo de Seguimiento','tipo',null,null,true,true,'','col-1');
+      $c[]=new cmp('cumplio','s','2',$e,$w.' '.$o,'cumplio','cumplio',null,null,true,true,'','col-1',"enbValue('cumplio','rt',2);");
+      $c[]=new cmp('observacion','a',50,'',$w.' rt '.$o,'Observación del Incumplimiento','observaciones',null,null,false,false,'','col-7');
+      for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
+      return $rta;
+  }
+
+  function get_compromiso(){
+    // var_dump($_REQUEST);
+    if($_REQUEST['id']==''){
+        return "";
+    }else{
+        $id=divide($_REQUEST['id']);
+        //  `fechaatencion`, `codigocups`, `finalidadconsulta`, `peso`, `talla`, `sistolica`, `diastolica`, `abdominal`, `brazo`, `diagnosticoprincipal`, `diagnosticorelacion1`, `diagnosticorelacion2`, `diagnosticorelacion3`, `fertil`, `preconcepcional`, `metodo`, `anticonceptivo`, `planificacion`, `mestruacion`, `gestante`, `gestaciones`, `partos`, `abortos`, `cesarias`, `vivos`, `muertos`, `vacunaciongestante`, `edadgestacion`, `ultimagestacion`, `probableparto`, `prenatal`, `fechaparto`, `rpsicosocial`, `robstetrico`, `rtromboembo`, `rdepresion`, `sifilisgestacional`, `sifiliscongenita`, `morbilidad`, `hepatitisb`, `vih`, `cronico`, `asistenciacronica`, `tratamiento`, `vacunascronico`, `menos5anios`, `esquemavacuna`, `signoalarma`, `cualalarma`, `dxnutricional`, `eventointeres`, `evento`, `cualevento`, `sirc`, `rutasirc`, `remision`, `cualremision`, `ordenpsicologia`, `ordenvacunacion`, `vacunacion`, `ordenlaboratorio`, `laboratorios`, `ordenimagenes`, `imagenes`, `ordenmedicamentos`, `medicamentos`, `rutacontinuidad`, `continuidad`, `relevo`  ON a.idpersona = b.idpersona AND a.tipodoc = b.tipo_doc
+        $sql="SELECT  compromiso
+        FROM hog_planconc
+        WHERE idcon ='{$id[1]}'";
+        // echo $sql;
+        $info=datos_mysql($sql);
+        return $info['responseResult'][0];			
+    }
+}
+
+  function gra_resultLab(){
+	$id=divide($_POST['idcom']);
+    // var_dump($id);
+    $info=datos_mysql("select equipo from usuarios where id_usuario='{$_SESSION['us_sds']}'");
+    if(isset($info['responseResult'][0])){ 
+      $equipo=$info['responseResult'][0]['equipo'];
+      $sql = "INSERT INTO hog_segcom VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+      $params = [
+        ['type' => 'i', 'value' => NULL ],
+        ['type' => 's', 'value' => $id[1]],
+        ['type' => 's', 'value' => $_POST['fechac']],
+        ['type' => 's', 'value' => $_POST['tipo']],
+        ['type' => 's', 'value' => $_POST['cumplio']],
+        ['type' => 's', 'value' => $_POST['observacion']],
+        ['type' => 's', 'value' => $equipo],
+         ['type' => 'i', 'value' => $_SESSION['us_sds']], // usu_create
+        ['type' => 's', 'value' => date("Y-m-d H:i:s")], // fecha_create
+        ['type' => 'i', 'value' => $_SESSION['us_sds']], // usu_update (ajustado)
+        ['type' => 's', 'value' => date("Y-m-d H:i:s")], // fecha_update (ajustado)
+        ['type' => 's', 'value' => 'A']
+      ];
+      $rta = mysql_prepd($sql, $params);
+      return $rta;
+    }else{
+      $rta="Error: msj['No existe un equipo actualmente para el usuario que realizo el seguimiento']";
+    }
+}
+
+
+function opc_cumplio($id=''){
+return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=170 and estado='A' ORDER BY 1",$id);
+}
+function opc_tipo($id=''){
+    return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=234 and estado='A' ORDER BY 1",$id);
+    }
 
 
 function formato_dato($a, $b, $c, $d) {
