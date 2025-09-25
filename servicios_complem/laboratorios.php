5a9dd1eb6b637ef88c9eeaf228a1c2660e144838
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-25 16:47:50 -0500

    09-25 Laborat 16:47

--- servicios_complem/laboratorios.php
+++ servicios_complem/laboratorios.php
@@ -34,7 +34,9 @@ function cap_menus($a,$b='cap',$con='con') {
   if ($a=='laboratorios' && isset($acc['crear']) && $acc['crear']=='SI') {  
    $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
   }
-  if($a=='resultLab')
+  if($a=='resultLab' && isset($acc['crear']) && $acc['crear']=='SI') {  
+   $rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
+  }
   $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";  
   return $rta;
 }
