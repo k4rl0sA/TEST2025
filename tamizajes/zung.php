7393431504ec6a2ae3ff8678bdc9896fd555ddcb
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-09-27 11:37:45 -0500

    09-27  FixZung 11;37

--- tamizajes/zung.php
+++ tamizajes/zung.php
@@ -184,7 +184,7 @@ function gra_tamzung(){
 	if(count($id)!= "2"){
 		return "No es posible actualizar el tamizaje";
 	}else{
-		$data=datos_mysql("select count(Z.momento) as moment from hog_tam_zung Z  where Z.idpeople='{$id[0]}'");
+		$data=datos_mysql("select count(Z.momento) as moment from hog_tam_zung Z  where Z.idpeople='{$id[0]}' and Z.estado='A'");
 		$momen=$data['responseResult'][0]['moment'];
 		if($momen=='0'){
 			$idmomento = 1;
