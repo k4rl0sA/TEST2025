83d4213a298cc62b968b99281fe3c1b98df0f519
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-06 09:26:59 -0500

    10-06  Ruteo 09:26

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -981,7 +981,7 @@ function formato_dato($a,$b,$c,$d){
 		$rta.="<li title='Crear Integrante Familia' Onclick=\"mostrar('person','pro',event,'','lib.php',7,'person');Color('famili-lis');\"><i class='fa-solid fa-person-circle-plus ico' id='".$c['Cod_Familiar']."'></i></li>";
 		$rta.="<li title='Mostrar Integrantes' Onclick=\"mostrar('person1','fix',event,'','lib.php',0,'person1');Color('famili-lis');\"><i class='fa-solid fa-people-group ico' id='".$c['Cod_Familiar']."'></i></li>";
 		$rta .= acceso('caract') ? "<li title='Crear Caracterización Familiar' onclick=\"mostrar('caract','pro',event,'','../crea-caract/lib.php',7,'caract');Color('famili-lis');\"><i class='fa-solid fa-file-circle-plus ico' id='{$c['Cod_Familiar']}'></i></li>" : "";
-		$rta .= acceso('planDcui') ? "<li title='Planes de Cuidado Familiar' onclick=\"mostrar('planDcui','pro',event,'','plancui.php',7);Color('famili-lis');\"><i class='fa-solid fa-file-contract ico' id='{$c['Cod_Familiar']}'></i></li>" : "";
+		$rta .= acceso('planDcui') ? "<li title='Planes de Cuidado de Bienestar Familiar' onclick=\"mostrar('planDcui','pro',event,'','plancui.php',7);Color('famili-lis');\"><i class='fa-solid fa-file-contract ico' id='{$c['Cod_Familiar']}'></i></li>" : "";
 		if(plan($c['Cod_Familiar'])===true){
 			$rta .= "<li title='Compromisos Concertados' onclick=\"mostrar('compConc','pro',event,'','plncon.php',7);Color('famili-lis');\"><i class='fa-solid fa-handshake-angle ico' id='{$c['Cod_Familiar']}'></i></li>";
 		}
