4657a15bb9e80d032ebd263d5bd076ae89c1c96b
Jcurrea91 <ingsoportesds1@gmail.com>
2025-10-08 10:10:43 -0500

    10-08 seguiremoto 10:10

--- ruteo1/seguiRemoto.php
+++ ruteo1/seguiRemoto.php
@@ -85,7 +85,7 @@ function cmp_seguiremoto(){
  
   $o='hab';
   $c[]=new cmp($o,'e',null,'SEGUIMIENTO REMOTO',$w);
-  $c[] = new cmp('gestante','s','3',$d,'',$w.' '.$o,'gestante','Gestante','gestante',null,null,false,$u,'','col-2');
+  $c[] = new cmp('gestante','s','3',$d,'',$w.' '.$o,'Gestante','gestante',null,null,false,$u,'','col-2');
   /*$c[] = new cmp('menor5','s','3',$d,'',$w.' '.$o,'menor5','Menor de 5 años','menor5',null,null,false,$u,'','col-2');
   $c[] = new cmp('cronico','s','3',$d,'',$w.' '.$o,'cronico','Usuario crónico','cronico',null,null,false,$u,'','col-2');
   $c[] = new cmp('general','s','3',$d,'',$w.' '.$o,'general','Usuario general','general',null,null,false,$u,'','col-2');
