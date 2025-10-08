7ec33a2520dc77274ea68eb382c1e6a68c41f274
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-08 10:42:57 -0500

    10-08 Ruteo 10:42

--- ruteo1/seguiRemoto.php
+++ ruteo1/seguiRemoto.php
@@ -121,7 +121,7 @@ function cmp_seguiremoto(){
 
   $o='acc';
   $c[]=new cmp($o,'e',null,'INFORMACIÓN ACCIONES',$w);
-  $c[] = new cmp('accion1','s','50',$d,$w.' '.$o,'accion1','Acción 1','accion1',null,null,false,$u,'','col-2','selectDepend(\'accion1\',\'desc_accion1\',\'../ruteo1/seguiRemoto.php\');');
+  /*$c[] = new cmp('accion1','s','50',$d,$w.' '.$o,'accion1','Acción 1','accion1',null,null,false,$u,'','col-2','selectDepend(\'accion1\',\'desc_accion1\',\'../ruteo1/seguiRemoto.php\');');
   $c[] = new cmp('desc_accion1','s','50',$d,$w.' '.$o,'desc_accion1','Descripción Acción 1','desc_accion1',null,null,false,$u,'','col-2');
   $c[] = new cmp('accion2','s','50',$d,$w.' '.$o,'accion2','Acción 2','accion2',null,null,false,$u,'','col-2','selectDepend(\'accion2\',\'desc_accion2\',\'../ruteo1/seguiRemoto.php\');');
   $c[] = new cmp('desc_accion2','s','50',$d,$w.' '.$o,'desc_accion2','Descripción Acción 2','desc_accion2',null,null,false,$u,'','col-2');
@@ -130,7 +130,7 @@ function cmp_seguiremoto(){
   $c[] = new cmp('observaciones','s','7000',$d,$w.' '.$o,'observaciones','Observaciones','observaciones',null,null,true,$u,'','col-12');
   $c[] = new cmp('continua_seguimiento','s','3',$d,$w.' '.$o,'continua_seguimiento','¿Continúa Seguimiento?','continua_seguimiento',null,null,true,$u,'','col-2');
   $c[] = new cmp('fecha_prox_seguimiento','d','10',$d,$w.' '.$o,'fecha_prox_seguimiento','Fecha Próximo Seguimiento','fecha_prox_seguimiento',null,null,false,$u,'','col-2');
-  $c[] = new cmp('motivo_no_continuidad','s','100',$d,$w.' '.$o,'motivo_no_continuidad','Motivo No Continuidad','motivo_no_continuidad',null,null,false,$u,'','col-2');
+  $c[] = new cmp('motivo_no_continuidad','s','100',$d,$w.' '.$o,'motivo_no_continuidad','Motivo No Continuidad','motivo_no_continuidad',null,null,false,$u,'','col-2');*/
      
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	return $rta;
