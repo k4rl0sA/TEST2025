f4e44b1006a65fb9ad9db06fdad38b58b779fa63
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-08 09:58:31 -0500

    10-08 Ruteo 09:58

--- ruteo1/seguiRemoto.php
+++ ruteo1/seguiRemoto.php
@@ -93,7 +93,7 @@ function cmp_seguiremoto(){
   $c[] = new cmp('gestante_cpn','s','3',$d,''$w.' '.$o,'gestante_cpn','Gestante asiste a CPN','gestante_cpn',null,null,false,$u,'','col-2');
   $c[] = new cmp('nov_pri_fam2','s','50',$d,''$w.' '.$o,'nov_pri_fam2','Novedad Prioridad Familiar 2','nov_pri_fam2',null,null,false,$u,'','col-2');
   
-  $c[] = new cmp('menor5_rpms','s','3',$d,''$w.' '.$o,'menor5_rpms','Menor 5 RPMS','menor5_rpms',null,null,false,$u,'','col-2');
+  /*$c[] = new cmp('menor5_rpms','s','3',$d,''$w.' '.$o,'menor5_rpms','Menor 5 RPMS','menor5_rpms',null,null,false,$u,'','col-2');
   $c[] = new cmp('menor5_riesgo','s','3',$d,''$w.' '.$o,'menor5_riesgo','Menor 5 Riesgo','menor5_riesgo',null,null,false,$u,'','col-2');
   $c[] = new cmp('nov_pri_fam3','s','50',$d,''$w.' '.$o,'nov_pri_fam3','Novedad Prioridad Familiar 3','nov_pri_fam3',null,null,false,$u,'','col-2');
   $c[] = new cmp('inasistente_12m','s','3',$d,''$w.' '.$o,'inasistente_12m','Inasistente 12 meses','inasistente_12m',null,null,false,$u,'','col-2');
@@ -131,7 +131,7 @@ function cmp_seguiremoto(){
   $c[] = new cmp('observaciones','s','7000',$d,''$w.' '.$o,'observaciones','Observaciones','observaciones',null,null,true,$u,'','col-12');
   $c[] = new cmp('continua_seguimiento','s','3',$d,''$w.' '.$o,'continua_seguimiento','¿Continúa Seguimiento?','continua_seguimiento',null,null,true,$u,'','col-2');
   $c[] = new cmp('fecha_prox_seguimiento','d','10',$d,''$w.' '.$o,'fecha_prox_seguimiento','Fecha Próximo Seguimiento','fecha_prox_seguimiento',null,null,false,$u,'','col-2');
-  $c[] = new cmp('motivo_no_continuidad','s','100',$d,''$w.' '.$o,'motivo_no_continuidad','Motivo No Continuidad','motivo_no_continuidad',null,null,false,$u,'','col-2');
+  $c[] = new cmp('motivo_no_continuidad','s','100',$d,''$w.' '.$o,'motivo_no_continuidad','Motivo No Continuidad','motivo_no_continuidad',null,null,false,$u,'','col-2');*/
      
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	return $rta;
