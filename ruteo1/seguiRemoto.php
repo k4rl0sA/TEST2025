ffbbfa1dfd65c34b91b6ad9db70452cd0b1f35c3
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-08 10:42:15 -0500

    10-08 Ruteo 10:42

--- ruteo1/seguiRemoto.php
+++ ruteo1/seguiRemoto.php
@@ -119,8 +119,6 @@ function cmp_seguiremoto(){
   $c[] = new cmp('deriva_perfil2','s','50',$d,$w.' '.$o,'Perfil Derivado 2','deriva_perfil2',null,null,false,$u,'','col-2');
   $c[] = new cmp('asignado_a2','s','50',$d,$w.' '.$o,'Asignado a 2','asignado_a2',null,null,false,$u,'','col-2');
 
-  
-  
   $o='acc';
   $c[]=new cmp($o,'e',null,'INFORMACIÓN ACCIONES',$w);
   $c[] = new cmp('accion1','s','50',$d,$w.' '.$o,'accion1','Acción 1','accion1',null,null,false,$u,'','col-2','selectDepend(\'accion1\',\'desc_accion1\',\'../ruteo1/seguiRemoto.php\');');
