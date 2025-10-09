15984b1f14385741c900410a25ec3f7a850ae779
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-09 15:15:25 -0500

    10-09 Ruteo 15:15

--- ruteo1/seguiRemoto.php
+++ ruteo1/seguiRemoto.php
@@ -332,6 +332,29 @@ function opc_nombre($id=''){
 	}
 	return "";
 }
+function opc_asignado_a1($id=''){
+	if(!empty($_REQUEST['perfil'])){
+		$perfil = $_REQUEST['perfil'];
+		$sql = "SELECT id_usuario, CONCAT(id_usuario, ' - ', nombre) as descripcion 
+				FROM usuarios 
+				WHERE perfil = '$perfil' AND estado = 'A' 
+				ORDER BY nombre";
+		return opc_sql($sql, $id);
+	}
+	return "";
+}
+function opc_asignado_a2($id=''){
+	if(!empty($_REQUEST['perfil'])){
+		$perfil = $_REQUEST['perfil'];
+		$sql = "SELECT id_usuario, CONCAT(id_usuario, ' - ', nombre) as descripcion 
+				FROM usuarios 
+				WHERE perfil = '$perfil' AND estado = 'A' 
+				ORDER BY nombre";
+		return opc_sql($sql, $id);
+	}
+	return "";
+}
+
 function opc_motivo_estado($id=''){
 return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=74 and estado='A' ORDER BY 1",$id);
 }
