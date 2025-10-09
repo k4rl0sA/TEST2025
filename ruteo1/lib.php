71d3b8062a28a3bcb5d941a4276f73c04446c930
Jcurrea91 <ingsoportesds1@gmail.com>
2025-10-09 10:00:08 -0500

    10-09 seguiremoto 10:00

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -618,7 +618,7 @@ function EnabFin($id) {
 	$sql = "SELECT COUNT(*) AS agenda FROM eac_ruteo g
 WHERE g.id_ruteo = {$id[0]} AND (g.estado_ruteo IS NULL or g.estado_ruteo = '')";
 	$info = datos_mysql($sql);
-	var_dump($info['responseResult'][0]["agenda"]);
+	//var_dump($info['responseResult'][0]["agenda"]);
 	if (intval($info['responseResult'][0]["agenda"]) > 0){
 		return true;
 	}else{
@@ -720,6 +720,7 @@ if ($a=='rute' && $b=='acciones'){
 		if (fin($c['ACCIONES'])) {
 			$rta.="<li class='icono efectividadAgenda' title='GESTIÓN FINAL' id='".$c['ACCIONES']."' Onclick=\"mostrar('ruteresol','pro',event,'','ruteoresolut.php',7,'ruteresol');\"></li>";
 		}
+		var_dump(EnabFin($c['ACCIONES']));
 		 if (EnabFin($c['ACCIONES'])) {
 			$rta .= acceso('reasigRut') ? "<li title='Reasignar Ruteo' onclick=\"mostrar('reasignar','pro',event,'','lib.php',7,'Reasignar Ruteo');\"><i class='fa-solid fa-route ico' id='{$c['ACCIONES']}'></i></li>" : "";
 		}
