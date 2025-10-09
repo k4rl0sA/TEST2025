2a2dda94c1693b0efa4ba99a15cb06e097de0e6b
Jcurrea91 <ingsoportesds1@gmail.com>
2025-10-09 10:00:51 -0500

    10-09 seguiremoto 10:00

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -720,7 +720,7 @@ if ($a=='rute' && $b=='acciones'){
 		if (fin($c['ACCIONES'])) {
 			$rta.="<li class='icono efectividadAgenda' title='GESTIÓN FINAL' id='".$c['ACCIONES']."' Onclick=\"mostrar('ruteresol','pro',event,'','ruteoresolut.php',7,'ruteresol');\"></li>";
 		}
-		var_dump(EnabFin($c['ACCIONES']));
+	var_dump(EnabFin($c['ACCIONES']));
 		 if (EnabFin($c['ACCIONES'])) {
 			$rta .= acceso('reasigRut') ? "<li title='Reasignar Ruteo' onclick=\"mostrar('reasignar','pro',event,'','lib.php',7,'Reasignar Ruteo');\"><i class='fa-solid fa-route ico' id='{$c['ACCIONES']}'></i></li>" : "";
 		}
