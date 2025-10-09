dcc2e224328c154563e3af3131cd906cbc1bdb70
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-09 14:52:31 -0500

    10-09 Ruteo 14:52

--- ruteo1/seguiRemoto.php
+++ ruteo1/seguiRemoto.php
@@ -173,10 +173,9 @@ function get_seguiremoto(){
     return "";
   }else{
     $id=divide($_POST['id']);
-     $sql="SELECT rm.id_ruteoremoto,rm.idruteoclas,rm.fecha_seg,rm.numsegui,rm.estado_s,rm.motivo_estado,rm.gestante,rm.menor5,rm.cronico,rm.general,rm.nov_pri_fam1,rm.gestante_cpn,rm.nov_pri_fam2,rm.menor5_rpms,rm.menor5_riesgo,rm.nov_pri_fam3,rm.inasistente_12m,rm.inasistente_6_12m,rm.persona_mayor,rm.persona_discapacidad,rm.salud_mental,rm.nuevo_diagnostico,rm.ningun_diagnostico,rm.vacunacion_incompleta,rm.acepta_vacunacion,rm.barrera_salud,rm.agendamiento,rm.activacion_ruta,rm.sin_afiliacion,rm.acepta_afiliacion,rm.sujeto_abordaje,rm.acepta_abordaje,rm.deriva_perfil1,rm.asignado_a1,rm.sujeto_concertacion,rm.acepta_plan,rm.deriva_perfil2,rm.asignado_a2,rm.accion1,rm.desc_accion1,rm.accion2,rm.desc_accion2,rm.accion3,rm.desc_accion3,rm.observaciones,rm.continua_seguimiento,rm.fecha_prox_seguimiento,rm.motivo_no_continuidad,r.priorizacion,r.tipo_prior
-        FROM ruteo_remoto rm
-        LEFT JOIN eac_ruteo_clas rc ON rm.idruteoclas = rc.id_rutclas
-        LEFT JOIN eac_ruteo r ON rc.idrutges = r.id_ruteo
+     $sql="SELECT CONCAT_WS('_',rm.id_ruteoremoto,rm.idruteoclas),r.priorizacion,r.tipo_prior,rm.fecha_seg,rm.numsegui,rm.estado_s,rm.motivo_estado,rm.gestante,rm.menor5,rm.cronico,rm.general,rm.nov_pri_fam1,rm.gestante_cpn,rm.nov_pri_fam2,rm.menor5_rpms,rm.menor5_riesgo,rm.nov_pri_fam3,rm.inasistente_12m,rm.inasistente_6_12m,rm.persona_mayor,rm.persona_discapacidad,rm.salud_mental,rm.nuevo_diagnostico,rm.ningun_diagnostico,rm.vacunacion_incompleta,rm.acepta_vacunacion,rm.barrera_salud,rm.agendamiento,rm.activacion_ruta,rm.sin_afiliacion,rm.acepta_afiliacion,rm.sujeto_abordaje,rm.acepta_abordaje,rm.deriva_perfil1,rm.asignado_a1,rm.sujeto_concertacion,rm.acepta_plan,rm.deriva_perfil2,rm.asignado_a2,rm.accion1,rm.desc_accion1,rm.accion2,rm.desc_accion2,rm.accion3,rm.desc_accion3,rm.observaciones,rm.continua_seguimiento,rm.fecha_prox_seguimiento,rm.motivo_no_continuidad
+       FROM ruteo_remoto rm
+        LEFT JOIN eac_ruteo r ON rm.idruteoclas=r.id_ruteo  
         WHERE rm.id_ruteoremoto='{$id[0]}'";
         var_dump($sql);
     $info=datos_mysql($sql);
