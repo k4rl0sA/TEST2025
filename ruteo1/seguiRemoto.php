bb28924396ccd0154aab01e5277d47966db2d098
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-09 13:56:07 -0500

    10-09 Ruteo 13:56

--- ruteo1/seguiRemoto.php
+++ ruteo1/seguiRemoto.php
@@ -282,6 +282,16 @@ function opc_pre_clasifclasificacion($id=''){
  function opc_perfil_gest($id=''){
 	return opc_sql('SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=218 and estado="A" AND descripcion=(select perfil from usuarios where id_usuario='.$_SESSION['us_sds'].') ORDER BY 1',$id);
 	}
+  function opc_deriva_perfil1asignado_a1($id=''){
+    if($_REQUEST['id']!=''){	
+            $sql = "SELECT id_usuario id,CONCAT(id_usuario,'-',nombre) usuario FROM usuarios right join apro_terr at ON id_usuario=at.doc_asignado WHERE 
+            perfil=(select descripcion from catadeta c where idcatalogo=218 and idcatadeta='{$_REQUEST['id']}' and estado='A') 
+            and id_usuario ='{$_SESSION['us_sds']}' ORDER BY nombre";
+            $info = datos_mysql($sql);		
+		 //return json_encode($sql);	
+           return json_encode($info['responseResult']);	
+        }
+}
   function opc_deriva_perfil2asignado_a2($id=''){
     if($_REQUEST['id']!=''){	
             $sql = "SELECT id_usuario id,CONCAT(id_usuario,'-',nombre) usuario FROM usuarios right join apro_terr at ON id_usuario=at.doc_asignado WHERE 
