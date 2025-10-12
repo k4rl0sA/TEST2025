6a2952d88777f6a4a71dcacd8ff0b52729b5151b
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-12 12:39:06 -0500

    10-12  FixLaboratorios  12:39

--- servicios_complem/laboratorios.php
+++ servicios_complem/laboratorios.php
@@ -263,6 +263,8 @@ function cmp_resultLab(){
       $t=['compromiso'=>''];
 	$d=get_respuesta();
 	if ($d==""){$d=$t;}
+  var_dump($_POST);
+  var_dump($_GET);
 	$days=fechas_app('vivienda');
       $c[]=new cmp($o,'e',null,'PLAN DE CUIDADO FAMILIAR CONCERTADO',$w);
       $c[]=new cmp('idrta','h',15,$_POST['id'],$w.' '.$key.' '.$o,'id','id',null,'####',false,false);
