81e03e0a05e3de1fee0020556229c9a46aa9c8f4
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-14 12:29:44 -0500

    10-14 FixAtenciones 12:29

--- atenciones/atencionMedi.php
+++ atenciones/atencionMedi.php
@@ -447,7 +447,7 @@ function cap_menus($a,$b='cap',$con='con') {
 		   $rta="<nav class='menu right'>";
 		   $rta.="<li class='icono editar ' title='Editar Atención' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,1000,'atencionM',event,this,['idpersona','tipo_doc'],'../atenciones/atencionMedi.php');
 		   setTimeout(getData,1300,'atencionM',event,this,['idpersona','tipo_doc'],'../atenciones/atencionMedi.php');		   
-		   Color('datos-lis');\"></li>";//
+		   Color('datos-lis');\"></li>";
 		   // setTimeout(changeSelect,1100,'letra1','rango1','../atenciones/atencionMedi.php');setTimeout(changeSelect,1150,'letra2','rango2','../atenciones/atencionMedi.php');setTimeout(changeSelect,1280,'letra3','rango3','../atenciones/atencionMedi.php');
 		   // setTimeout(changeSelect,1385,'rango1','diagnostico1','../atenciones/atencionMedi.php');setTimeout(changeSelect,1385,'rango2','diagnostico2','../atenciones/atencionMedi.php');setTimeout(changeSelect,1385,'rango3','diagnostico3','../atenciones/atencionMedi.php');
 			//$rta.="<li class='icono editar ' title='Generar Consulta' id='".$c['ACCIONES']."' Onclick=\"setTimeout(getData,1000,'atencion',event,this,['idpersona','tipo_doc'],'../atenciones/atencionMedi.php');Color('datos-lis');\"></li>";
