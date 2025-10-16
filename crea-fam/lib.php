dd8f9780cf0abc978ab8b0948800e2fbcdc1de49
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-16 15:52:40 -0500

    10-16 FixSoporte 15:52

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -1083,6 +1083,7 @@ function formato_dato($a,$b,$c,$d){
 			$rta .= acceso('servagen') ? "<li title='Servicio de Agendamiento' onclick=\"mostrar('servagen','pro',event,'','../agendamient/serage.php',7,'Servicio de Agendamiento');Color('datos-lis');\"><i class=' fa-solid fa-square-phone ico' id='{$c['ACCIONES']}'></i></li>":"";
 
 			$rta .="<li title='Barreras' Onclick=\"mostrar('barreras','pro',event,'','../tamizajes/barreras.php',7,'barreras');Color('datos-lis');\"><i class='fa-solid f-road-barrier color-soporte ico' id='".$c['ACCIONES']."'></i> </li>";
+			$rta.="<li title='Trasladar Usuario' Onclick=\"mostrar('traslados','pro',event,'','../soporte/trasladloc.php',4,'traslados');Color('datos-lis');\"><i class='fa-solid fa-reply-all color-soporte ico' id='".$c['ACCIONES']."'></i> </li>";
 
 			if (is_array($tamiz) && in_array('tamvalories', $tamiz)) {
 				$rta .= acceso('tamvalories') ? "<li title='Valoración del Riesgo' onclick=\"mostrar('tamvalories','pro',event,'','../tamizajes/valoriesg.php',7,'Valoración del Riesgo');Color('datos-lis');\"><i class='fa-solid fa-skull-crossbones ico' id='{$c['ACCIONES']}'></i></li>":"";
