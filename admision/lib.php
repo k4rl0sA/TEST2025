147396e4958bfc2c97d88012263239e84fa96394
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-16 15:57:11 -0500

    10-16 FixSoporte 15:57

--- admision/lib.php
+++ admision/lib.php
@@ -33,7 +33,7 @@ function lis_adm(){
 	FROM `adm_facturacion` F WHERE idpeople ='{$id[0]}' and estado!='I' ";
 	$sql.=" ORDER BY F.fecha_create";
 	$sql.=' LIMIT '.$pag.','.$regxPag;
-	echo $sql;
+	//  echo $sql;
 	$datos=datos_mysql($sql);
 	return create_table($total,$datos["responseResult"],"adm",$regxPag,'lib.php');
 }
@@ -53,7 +53,7 @@ function lis_admision(){
 	$sql.=whe_admision();
 	$sql.=" ORDER BY A.fecha_create";
 	$sql.=' LIMIT '.$pag.','.$regxPag;
-	//  echo $sql;
+	 echo $sql;
 		$datos=datos_mysql($sql);
 	return create_table($total,$datos["responseResult"],"admision",$regxPag);
 	}
