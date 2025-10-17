978c69adac37c33ed179f6c12be24c1019ec70ed
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-17 10:29:16 -0500

    10-17 Barreras 10:29

--- tamizajes/barreras.php
+++ tamizajes/barreras.php
@@ -167,6 +167,9 @@ function opc_fis_limitacion_movilidad($id=''){
 function opc_fis_usa_dispositivo_asistencia($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=311 and estado='A' ORDER BY 1",$id);
 }
+function opc_fis_usa_dispositivo_asistencia($id=''){
+    return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=311 and estado='A' ORDER BY 1",$id);
+} 
 // Menú y encabezado
 function focus_barreras(){
     return 'barreras';
