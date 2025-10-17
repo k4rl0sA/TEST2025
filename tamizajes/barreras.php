eeec4641ee4da9309e5981162e3ad3b19aba5934
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-17 12:31:11 -0500

    10-17 Barreras 12:31

--- tamizajes/barreras.php
+++ tamizajes/barreras.php
@@ -160,13 +160,14 @@ function opc_geo_dificultad_acudir($id=''){
 function opc_fis_dispositivo_asistencia_cual($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=302 and estado='A' ORDER BY 1",$id);
 }
+function opc_eco_limitacion_cual($id=''){
+    return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=303 and estado='A' ORDER BY 1",$id);
+}
 /* function opc_geo_dificultad_acudir_cual($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=301 and estado='A' ORDER BY 1",$id);
 }
 
-function opc_eco_limitacion_cual($id=''){
-    return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=303 and estado='A' ORDER BY 1",$id);
-}
+
 function opc_adm_estado_afiliacion($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=304 and estado='A' ORDER BY 1",$id);
 }
