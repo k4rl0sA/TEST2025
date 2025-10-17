9d2042bb74caafbf85cc7f5a821096d7a1d54e7d
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-17 12:38:38 -0500

    10-17 Barreras 12:38

--- tamizajes/barreras.php
+++ tamizajes/barreras.php
@@ -155,19 +155,19 @@ function opc_sexo($id=''){
 	return opc_sql("SELECT `idcatadeta`,descripcion FROM `catadeta` WHERE idcatalogo=21 and estado='A' ORDER BY 1",$id);
 }
 function opc_geo_dificultad_acudir($id=''){
-    return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=309 and estado='A' ORDER BY 1",$id);
-}
-function opc_fis_dispositivo_asistencia_cual($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=302 and estado='A' ORDER BY 1",$id);
 }
-function opc_eco_limitacion_cual($id=''){
+function opc_fis_dispositivo_asistencia_cual($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=303 and estado='A' ORDER BY 1",$id);
 }
-function opc_adm_estado_afiliacion($id=''){
+function opc_eco_limitacion_cual($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=304 and estado='A' ORDER BY 1",$id);
 }
-function opc_adm_dificultad_citas_cual($id=''){
+function opc_adm_estado_afiliacion($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=305 and estado='A' ORDER BY 1",$id);
+}
+function opc_adm_dificultad_citas_cual($id=''){
+    return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=306 and estado='A' ORDER BY 1",$id);
 }
 
 // Menú y encabezado
