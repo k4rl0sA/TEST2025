6a0dadd567180096d56d0586611bb6d08ccb49ea
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-17 10:28:01 -0500

    10-17 Barreras 10:27

--- tamizajes/barreras.php
+++ tamizajes/barreras.php
@@ -161,6 +161,9 @@ function opc_geo_dificultad_salir($id=''){
 function opc_geo_dificultad_acudir($id=''){
     return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=309 and estado='A' ORDER BY 1",$id);
 }
+function opc_fis_limitacion_movilidad($id=''){
+    return opc_sql("SELECT idcatadeta,descripcion FROM catadeta WHERE idcatalogo=310 and estado='A' ORDER BY 1",$id);
+}
 // Menú y encabezado
 function focus_barreras(){
     return 'barreras';
