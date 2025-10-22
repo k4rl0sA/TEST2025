3f1e7e066d2ddbf46d75ec03418f7b0be6bd576e
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-22 18:02:29 -0500

    10-21 Soledad 18:02

--- ruteo1/lib.php
+++ ruteo1/lib.php
@@ -682,8 +682,8 @@ function opc_perfil_gest($id=''){
 	}
   function opc_perfil_gestusuario_gest($id=''){
     if($_REQUEST['id']!=''){	
-            $sql = "SELECT id_usuario id,CONCAT(id_usuario,'-',nombre) usuario FROM usuarios right join apro_terr at ON id_usuario=at.doc_asignado WHERE 
-            perfil=(select descripcion from catadeta c where idcatalogo=218 and idcatadeta='{$_REQUEST['id']}' and estado='A') 
+            $sql = "SELECT id_usuario id,CONCAT(id_usuario,'-',nombre) usuario FROM usuarios 
+			WHERE perfil=(select descripcion from catadeta c where idcatalogo=218 and idcatadeta='{$_REQUEST['id']}' and estado='A') 
             and id_usuario ='{$_SESSION['us_sds']}' ORDER BY nombre";
             $info = datos_mysql($sql);		
 		 //return json_encode($sql);	
