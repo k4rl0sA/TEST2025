a07d2ae308f64477907d085871b7775eac27b657
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2025-10-24 18:04:07 -0500

    20251024_fer

--- th/lib.php
+++ th/lib.php
@@ -190,7 +190,7 @@ function formato_dato($a, $b, $c, $d) {
                 'evento' => "mostrar('th','{$c['ACCIONES']}',this,'lib.php',1);"
             ],
             'ver' => [
-                'icono' => '<fa-solid fa-clipboard-list',
+                'icono' => 'fa-solid fa-clipboard-list',
                 'clase' => 'ico',
                 'title' => 'Ver Detalles',
                 'permiso' => acceso('th'),
