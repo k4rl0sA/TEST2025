adffa71514b2387de3797c11bca84293399a36f7
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2025-10-24 16:24:45 -0500

    20251024_fer

--- th/lib.php
+++ th/lib.php
@@ -89,38 +89,18 @@ function cmp_th(){
 // var_dump($_REQUEST);
 // var_dump($d);
  $o='segrep';
- $c[]=new cmp($o,'e',null,'CASO REPORTADO',$w);
- $c[]=new cmp('id','h','20','',$w.' '.$o,'','',null,null,true,false,'','col-1');
- $c[]=new cmp('fecha_asig','d','10','',$w.' '.$o,'FECHA ASIGNACIÓN','fecha_asig',null,null,false,false,'','col-15');
- $c[]=new cmp('fuente','s','3','',$w.' '.$o,'FUENTE O REMITENTE','fuente',null,null,false,false,'','col-25');
- $c[]=new cmp('priorizacion','s','3','',$w.' '.$o,'COHORTE DE RIESGO','priorizacion',null,null,false,false,'','col-3');
- $c[]=new cmp('tipo_prior','s','3','',$w.' '.$o,'GRUPO DE POBLACION PRIORIZADA','tipo_prior',null,null,false,false,'','col-3');
-
+ $c[]=new cmp('id','h',15,$_POST['id'],$w.' '.$o,'id','id',null,'####',false,false);
  $c[]=new cmp('tipo_doc','s','3','',$w.' '.$o,'TIPO DE DOCUMENTO','tipo_doc',null,null,false,false,'','col-2');
  $c[]=new cmp('documento','nu','999999999999999999','',$w.' '.$o,'NÚMERO DE DOCUMENTO','documento',null,null,false,false,'','col-2');
- $c[]=new cmp('nombres','t','80','',$w.' '.$o,'NOMBRES Y APELLIDOS DEL USUARIO','nombres',null,null,false,false,'','col-4');
- $c[]=new cmp('sexo','s','3','',$w.' '.$o,'SEXO','sexo',null,null,false,false,'','col-2');
+ $c[]=new cmp('nombre1','t','30',$d['nombre1'],$w.' '.$o,'Primer Nombre','nombre1',null,null,true,true,'','col-2');
+ $c[]=new cmp('nombre2','t','30',$d['nombre2'],$w.' '.$o,'Segundo Nombre','nombre2',null,null,false,true,'','col-2');
+ $c[]=new cmp('apellido1','t','30',$d['apellido1'],$w.' '.$o,'Primer Apellido','apellido1',null,null,true,true,'','col-2');
+ $c[]=new cmp('apellido2','t','30',$d['apellido2'],$w.' '.$o,'Segundo Apellido','apellido2',null,null,false,true,'','col-2');
+ $c[]=new cmp('fecha_nacimiento','d','',$d['fecha_nacimiento'],$w.' '.$o,'Fecha de nacimiento','fecha_nacimiento',null,null,true,$edit,'','col-2',"validDate(this,-43800,0);",[],"child14('fecha_nacimiento','osx');Ocup5('fecha_nacimiento','OcU');");
+ $c[]=new cmp('sexo','s','3',$d['sexo'],$w.' '.$o,'Sexo','sexo',null,null,true,$edit,'','col-2');
+ $c[]=new cmp('contacto','n','10','',$w.' '.$o,'N° Contacto','contacto',null,null,false,false,'','col-2');
+ $c[]=new cmp('email','n','10','',$w.' '.$o,'Correo Electronico','email',null,null,false,false,'','col-2');
  
- $o='datcon';
- //$c[]=new cmp($o,'e',null,'DATOS DE CONTACTO',$w);
- $c[]=new cmp('direccion','t','90','',$w.' '.$o,'Direccion','direccion',null,null,false,false,'','col-4');
- $c[]=new cmp('telefono1','n','10','',$w.' '.$o,'Telefono 1','telefono1',null,null,false,false,'','col-2');
- $c[]=new cmp('telefono2','n','10','',$w.' '.$o,'Telefono 2','telefono2',null,null,false,false,'','col-2');
- $c[]=new cmp('telefono3','n','10','',$w.' '.$o,'Telefono 3','telefono3',null,null,false,false,'','col-2');
-
- $c[]=new cmp('subred_report','s','3','',$w.' '.$o,'Subred','subred_report',null,null,false,false,'','col-3');
- $c[]=new cmp('localidad','s','3','',$w.' '.$o,'Localidad','localidad',null,null,false,false,'','col-2');
- $c[]=new cmp('upz','s','3','',$w.' '.$o,'Upz','upz',null,null,false,false,'','col-2');
- $c[]=new cmp('barrio','s','5','',$w.' '.$o,'Barrio','barrio',null,null,false,false,'','col-3');
- $c[]=new cmp('sector_catastral','n','6','',$w.' '.$o,'Sector Catastral (6)','sector_catastral',null,null,false,false,'','col-25');
- $c[]=new cmp('nummanzana','n','3','',$w.' '.$o,'Nummanzana (3)','nummanzana',null,null,false,false,'','col-25');
- $c[]=new cmp('predio_num','n','3','',$w.' '.$o,'Predio de Num (3)','predio_num',null,null,false,false,'','col-25');
- $c[]=new cmp('unidad_habit','n','4','',$w.' '.$o,'Unidad habitacional (3)','unidad_habit',null,null,false,false,'','col-25');
- $c[]=new cmp('cordx','t','15','',$w.' '.$o,'Cordx','cordx',null,null,false,false,'','col-5');
- $c[]=new cmp('cordy','t','15','',$w.' '.$o,'Cordy','cordy',null,null,false,false,'','col-5');
- 
-
-//  $c[]=new cmp('observacion','a',50,$d['obse'],$w.' '.$o,'Observacion','observacion',null,null,true,true,'','col-10');
   
  for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();¨*/
  return $rta;
