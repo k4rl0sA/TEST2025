7f0915c087b503cb038c0654c973bb88b001b094
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2025-10-24 17:02:23 -0500

    20251024_fer

--- th/lib.php
+++ th/lib.php
@@ -84,7 +84,7 @@ $rta="";
  $c[]=new cmp('fecha_nacimiento','d','','',$w.' '.$o,'Fecha de nacimiento','fecha_nacimiento',null,null,true,false,'','col-2');
  $c[]=new cmp('sexo','t','3','',$w.' '.$o,'Sexo','sexo',null,null,true,false,'','col-2');
  $c[]=new cmp('contacto','n','10','',$w.' '.$o,'N° Contacto','contacto',null,null,false,false,'','col-2');
- $c[]=new cmp('email','n','10','',$w.' '.$o,'Correo Electronico','email',null,null,false,false,'','col-2'); */
+ $c[]=new cmp('email','n','10','',$w.' '.$o,'Correo Electronico','email',null,null,false,false,'','col-2'); 
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	return $rta;
 }
