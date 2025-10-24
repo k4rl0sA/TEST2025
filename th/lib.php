def24f14ed86b9a225a2e44a402d236bbdc10b49
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-24 16:44:56 -0500

    10-24 TH 16:44

--- th/lib.php
+++ th/lib.php
@@ -75,7 +75,8 @@ function cmp_th(){
  $w='th';
  $o='segrep';
  $c[]=new cmp('id','h',15,$_POST['id'],$w.' '.$o,'id','id',null,'####',false,false);
- $c[]=new cmp('tipo_doc','s','3','',$w.' '.$o,'TIPO DE DOCUMENTO','tipo_doc',null,null,false,false,'','col-2');
+ $c[]=new cmp('tipo_doc','s',3,'',$w.' '.$o,'Tipo de Documento','tipo_doc',null,'####',false,false,'','col-2');
+//  $c[]=new cmp('tipo_doc','s','3','',$w.' '.$o,'TIPO DE DOCUMENTO','tipo_doc',null,null,false,false,'','col-2');
  /* $c[]=new cmp('documento','nu','999999999999999999','',$w.' '.$o,'NÚMERO DE DOCUMENTO','documento',null,null,false,false,'','col-2');
  $c[]=new cmp('nombre1','t','30','',$w.' '.$o,'Primer Nombre','nombre1',null,null,true,true,'','col-2');
  $c[]=new cmp('nombre2','t','30','',$w.' '.$o,'Segundo Nombre','nombre2',null,null,false,true,'','col-2');
