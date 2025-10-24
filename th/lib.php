40e31ead89d79d2ec29f1c6980a3416fd15d08c7
Ferchokar <114622263+Ferchokar@users.noreply.github.com>
2025-10-24 16:08:05 -0500

    20251024_fer

--- th/lib.php
+++ th/lib.php
@@ -21,7 +21,7 @@ else {
 function lis_th(){
 $info = datos_mysql("SELECT COUNT(*) total FROM th T WHERE " . whe_th());
     $total = $info['responseResult'][0]['total'];
-    $regxPag = 10;
+    $regxPag = 20;
     $pag = (isset($_POST['pag-th'])) ? ($_POST['pag-th'] - 1) * $regxPag : 0;
 
     $sql = "SELECT T.tipo_doc AS 'Tipo Documento', T.n_documento AS 'N° Documento', concat (T.nombre1, ' ', T.nombre2, ' ', T.apellido1, ' ', T.apellido2) AS 'Nombres y Apellidos del Colaborador', T.n_contacto AS 'N° Contacto', T.estado AS 'Estado Usuario' 
