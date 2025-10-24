bf83b33a1e94058da16e7a7d6bf9aded744d7655
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-24 17:59:01 -0500

    10-24 Discapacidad 17:58

--- th/index.php
+++ th/index.php
@@ -8,7 +8,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/libs/nav.php';
 <title>Talento Humano || <?php echo $APP; ?></title>
 <link href="../libs/css/stylePop.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Cabin+Sketch&family=Chicle&family=Merienda&family=Rancho&family=Boogaloo&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.1.0/css/all.min.css">
 <script src="../libs/js/a.js"></script>
 <script src="../libs/js/x.js?v=28.0"></script>
 <script src="../libs/js/d.js"></script>
