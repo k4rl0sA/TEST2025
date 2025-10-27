265c54450cc2160b294fe718d079a18df683f887
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-27 10:54:44 -0500

    10-27 Discapacidad 10:54

--- th/lib.php
+++ th/lib.php
@@ -87,7 +87,7 @@ if ($d==""){$d=$t;}
  $c[]=new cmp('fecha_nacimiento','d','',$d['fecha_nacimiento'],$w.' '.$o,'Fecha de nacimiento','fecha_nacimiento',null,null,true,true,'','col-25',"validDate(this,-43800,0);");
  $c[]=new cmp('sexo','s','3',$d['sexo'],$w.' '.$o,'Sexo','sexo',null,null,true,true,'','col-25');
  $c[]=new cmp('contacto','nu','9999999999',$d['contacto'],$w.' '.$o,'N° Contacto','contacto','rgxphone',null,true,true,'','col-25');
- $c[]=new cmp('email','em','50',$d['email'],$w.' '.$o,'Correo Electronico','email',null,null,true,true,'','col-25'); 
+ $c[]=new cmp('email','em','50',$d['email'],$w.' '.$o,'Correo Electronico','email','rgxmail',null,true,true,'','col-25'); 
 	for ($i=0;$i<count($c);$i++) $rta.=$c[$i]->put();
 	return $rta;
 }
