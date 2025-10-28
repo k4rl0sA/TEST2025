04670ad74beb21b2e1e94232cdda4f4b228005f0
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-28 10:06:47 -0500

    10-28 TH 10:06

--- vsp/discapacidad1.php
+++ vsp/discapacidad1.php
@@ -2,6 +2,7 @@
 require_once "../libs/gestion.php";
 //MOSTRAR TODOS LOS ERRORES Y WARNINGS 
 error_reporting(E_ALL);
+ini_set('display_startup_errors','1');
 ini_set('display_errors','1');
 if ($_POST['a']!='opc') $perf=perfil($_POST['tb']);
 if (!isset($_SESSION['us_sds'])) die("<script>window.top.location.href='/';</script>");
