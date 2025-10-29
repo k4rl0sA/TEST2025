2ae67f9c8be899bf340ea68d0dd942598f0cdbba
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-29 13:43:41 -0500

    10-29 TH 13:43

--- th/actividades.php
+++ th/actividades.php
@@ -100,6 +100,17 @@ function cmp_actividades(){
     if ($d == "") { $d = $t; }
     $w = 'actividades';
     $o = 'actividadinfo';
+    $rta .= "<div class="search-wrapper">
+    <input type="number" 
+           class="number-input-svg" 
+           placeholder="Número a buscar..."
+           step="0.1">
+    <button class="search-btn-svg">
+        <svg class="search-icon-svg" viewBox="0 0 24 24">
+            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+        </svg>
+    </button>
+</div>";
     $c[] = new cmp($o,'e',null,'INFORMACIÓN DE LA ACTIVIDAD',$w);
     $c[] = new cmp($o,'l',null,'',$w);
     $c[] = new cmp('id','h',15,$_POST['id'] ?? '',$w.' '.$o,'id','id',null,'####',false,false);
