17a17adec68c7c59b0df0bae729c14819d426039
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-29 13:11:03 -0500

    10-29 TH 13:11

--- th/actividades.php
+++ th/actividades.php
@@ -92,7 +92,7 @@ function cap_menus($a,$b='cap',$con='con') {
 }
 
 function cmp_actividades(){
-/*    $rta = "";
+    $rta = "";
 	$rta .="<div class='encabezado vivienda'>ACTIVIDADES REALIZADAS</div><div class='contenido' id='gestion-lis' >".lis_actividades()."</div></div>";
      $t = ['id_thact' => '', 'actividad' => '','perreq' => '' ,'rol' => '', 'acbi' => '','sudacbi' => '', 'actbien' => '', 'hora_act' => '', 'hora_th' => '','per_ano' => '', 'per_mes' => '', 'can_act' => '', 'total_horas' => '', 'total_valor' => ''];
     // $d='';
@@ -148,7 +148,6 @@ function cmp_actividades(){
     </script>";
      
     return $rta;
-    */
 }
 
 function get_actividades(){
