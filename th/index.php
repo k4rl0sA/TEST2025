c4c058af4f7c48dbb13eacda9a4434f86a975727
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-10-31 12:15:02 -0500

    10-31 TH 12:15

--- th/index.php
+++ th/index.php
@@ -52,7 +52,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/libs/nav.php';
     fill: #666;
 }
 </style>
-<script src="../libs/js/a.js?v=12"></script>
+<script src="../libs/js/a.js?v=13"></script>
 <script src="../libs/js/x.js?v=32.0"></script>
 <script src="../libs/js/d.js"></script>
 <script src="../libs/js/popup.js"></script>
