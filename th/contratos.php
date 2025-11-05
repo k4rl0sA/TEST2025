4577666691cb5a49386075510476ef75763a483a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-05 14:42:56 -0500

    11-05 FixActividTH 14:42

--- th/contratos.php
+++ th/contratos.php
@@ -212,14 +212,14 @@ function opc_tipo_expe($id=''){
 }
 
 function ajustar($id){
-    $idth = idReal($id ?? '', $_SESSION['hash'] ?? [], '_contratos');
+  /*   $idth = idReal($id ?? '', $_SESSION['hash'] ?? [], '_contratos');
     $sql]="SELECT COUNT(*) AS total FROM th_contratos WHERE id_thcon='$idth' and ajustar=1 AND estado='A'";
     $info = datos_mysql($sql);
     if($info['responseResult'][0]['total']>0){
         return true;
     }else{
         return false;
-    } 
+    }  */
 }
 
 function formato_dato($a, $b, $c, $d){
