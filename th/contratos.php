252ce47c481f5c6712dbee841994931c5bb3f5ba
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-05 16:16:03 -0500

    11-05 FixActividTH 16:16

--- th/contratos.php
+++ th/contratos.php
@@ -239,7 +239,11 @@ function formato_dato($a, $b, $c, $d){
         
         foreach ($accionesDisponibles as $key => $accion) {
             // var_dump($accion['hash']);
+            //mostrar los valores reales de los hashes
+            var_dump($accion['hash']);
+
             var_dump(idReal($accion['hash'], $_SESSION['hash'] ?? [], '_contratos'));
+
             if (ajustar($accion['hash'])) {
                 if ($accion['permiso']) {
                     limpiar_hashes();
