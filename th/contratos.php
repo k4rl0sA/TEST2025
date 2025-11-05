3bd3cea860431cf509de28b713c1fd29b0504fa7
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-05 16:10:18 -0500

    11-05 FixActividTH 16:10

--- th/contratos.php
+++ th/contratos.php
@@ -194,7 +194,7 @@ function opc_tipo_expe($id=''){
 function ajustar($id){
     $hash = $id ?? '';
     $session_hash = $_SESSION['hash'] ?? [];
-    $suffixes = ['_contratos','_th','_editar'];
+    $suffixes = ['_contratos'];
     $idth = null;
     // Intentar resolver el id real probando varios sufijos
     foreach ($suffixes as $sufijo) {
