236278de8ae91d22df8f950f1549298069d1535a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-14 15:26:24 -0500

    11-14 Feminicidio 15:26

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -796,7 +796,7 @@ function eventAsign($key) {
 	  35 => ['icono' => 'cancer', 'titulo' => 'CANCER DE COLON Y RECTO', 'modulo' => 'cancinfa'],
 	  36 => ['icono' => 'cancer', 'titulo' => 'CANCER DE PIEL', 'modulo' => 'cancinfa'],
 	  37 => ['icono' => 'cancer', 'titulo' => 'CANCER DE ESTOMAGO', 'modulo' => 'cancinfa'],
-	  38 => ['icono' => 'femini', 'titulo' => 'ACOMPAÑAMIENTO VBG', 'modulo' =>($perfilUsuario == 'PSICOLOGIA') ? 'feminicidio1' : 'feminicidio'],
+	  38 => ['icono' => 'femin1', 'titulo' => 'ACOMPAÑAMIENTO VBG', 'modulo' =>($perfilUsuario == 'PSICOLOGIA') ? 'feminicidio1' : 'feminicidio'],
 	  39 => ['icono' => 'soledad', 'titulo' => 'MAS COMPAÑIA MAS BIENESTAR', 'modulo' => 'soledad'],
     );
     $eve = '';
