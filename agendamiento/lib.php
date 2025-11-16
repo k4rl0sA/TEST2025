3453eae82242d4127e487ac81af7f3ba792ed2e0
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-16 00:44:08 -0500

    11-16 FixAgendamiento 00:44

--- agendamiento/lib.php
+++ agendamiento/lib.php
@@ -196,12 +196,8 @@ function cap_menus($a,$b='cap',$con='con') {
   $acc=rol($a);
   if($a=='agendamiento' && isset($acc['crear']) && $acc['crear']=='SI'){
   	$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
-  }
-  if($a=='confirma_asistencia' && isset($acc['crear']) && $acc['crear']=='SI'){
-  	$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
-  }
-  if($a=='seguimiento' && isset($acc['crear']) && $acc['crear']=='SI'){
-  	$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
+  }else{
+	$rta .= "<li class='icono $a grabar'      title='Grabar'          OnClick=\"grabar('$a',this);\"></li>";
   }
   $rta .= "<li class='icono $a actualizar'  title='Actualizar'      Onclick=\"act_lista('$a',this);\"></li>";
   return $rta;
