9c70404a32a8aaaf07c7f6068194d8715d350266
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-17 02:12:26 -0500

    11-17 FixAjustesCaract  02:12

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -150,7 +150,7 @@ function cap_menus($a,$b='cap',$con='con') {
 }
 
 function lis_famili(){
-	var_dump($_POST);
+	// var_dump($_POST);
 	var_dump($_REQUEST);
 	// $id=divide($_POST['id']);
 	$sql="SELECT idpre ACCIONES,id_fam AS Cod_Familiar,numfam AS N°_FAMILIA,fecha,CONCAT_WS(' ',FN_CATALOGODESC(6,complemento1),nuc1,FN_CATALOGODESC(6,complemento2),nuc2,FN_CATALOGODESC(6,complemento3),nuc3) Complementos,
