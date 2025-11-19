a23056a533f0e05a7e3cc609c952a5d59e4b44ca
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-19 14:35:27 -0500

    11-19 Fix107 14:35

--- crea-fam/lib.php
+++ crea-fam/lib.php
@@ -801,7 +801,7 @@ function eventAsign($key) {
 	  35 => ['icono' => 'cancer', 'titulo' => 'CANCER DE COLON Y RECTO', 'modulo' => 'cancinfa'],
 	  36 => ['icono' => 'cancer', 'titulo' => 'CANCER DE PIEL', 'modulo' => 'cancinfa'],
 	  37 => ['icono' => 'cancer', 'titulo' => 'CANCER DE ESTOMAGO', 'modulo' => 'cancinfa'],
-	  38 => ['icono' => 'femin1', 'titulo' => 'ESTRATEGIA (107) | VBG | ENF', 'feminicidio'],
+	  38 => ['icono' => 'femin1', 'titulo' => 'ESTRATEGIA (107) | VBG | ENF','modulo' => 'feminicidio'],
 	  39 => ['icono' => 'duelo1', 'titulo' => 'MAS COMPAÑIA MAS BIENESTAR', 'modulo' => 'soledad'],
 	  40 => ['icono' => 'femin1', 'titulo' => 'ESTRATEGIA (107) | VBG | PSI', 'modulo' => 'feminicidio1'],
 	  41 => ['icono' => 'discap', 'titulo' => 'RBC - REL', 'modulo' => 'discapacidad1']
