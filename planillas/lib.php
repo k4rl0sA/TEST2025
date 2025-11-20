716756fec60133b0a7896c42692bd9ad3af52e73
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-20 15:06:32 -0500

    11-20 FixPlanillas 15:06

--- planillas/lib.php
+++ planillas/lib.php
@@ -369,7 +369,7 @@ function opc_tipo_doc($id=''){
 function gra_planillas(){
     $sql1="SELECT idpeople,vivipersona
 		FROM `person` 
-   	WHERE idpersona ='".$_POST['idpeople']."' AND tipo_doc='".$_POST['tipo']."' AND estado='A'";
+   	WHERE idpersona ='".$_POST['idpersona']."' AND tipo_doc='".$_POST['tipo']."' AND estado='A'";
 	$info=datos_mysql($sql1);
     if ($info['responseResult']) {
         $codfam=$info['responseResult'][0]['vivipersona'];
