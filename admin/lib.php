00c492368e20eb48748e7ef91d69bf9c433cee2a
k4rl0sA <71453079+k4rl0sA@users.noreply.github.com>
2025-11-25 12:21:02 -0500

    11-25 FixPlanos 12:20

--- admin/lib.php
+++ admin/lib.php
@@ -2528,7 +2528,7 @@ function lis_agendamiento($txt){
 	P.fecha_nacimiento 'Fecha de Nacimiento',P.sexo, A.idagendamiento AS 'Cos Registro', FN_CATALOGODESC(274,A.punto_atencion ) AS 'Punto de Atención',FN_CATALOGODESC(275,A.tipo_cita) 'Tipo de Cita',
 	A.fecha_create 'Fecha de Asignación',A.fecha_cita 'Fecha de la Cita',A.hora_cita 'Hora de la Cita',A.nombre_atendio 'Nombre quien Atendió Llamada',A.usu_creo 'Digitador',A.observac_cita 'Observación Cita',IFNULL(A.fecha_llamada,'00-00-0000') 'Fecha Recordación',
 	ifnull(A.nombre_llamada,'-') 'Nombre quien Recibió Llamada' ,ifnull(A.confirma_cita,'-') 'Confirmo Cita',ifnull(A.msjtxt,'-') 'Desea Envio de Msj',
-	ifnull(A.usu_update,'-') 'Digitador1',ifnull(A.observac_llamadas,'-') 'Observaciones de Recordación',ifnull(A.fecha_llamada2,'-') 'Fecha Llamada por Efectividad',ifnull(A.nombre_llamada2,'-') 'Nombre quien Contesto Llamada',ifnull(FN_CATALOGODESC(41,A.motivo_inasistencia),'-') 'Motivo de la Inasistencia',ifnull(A.reasigno,'-') 'Se reasigno la Cita',ifnull(A.usu_update,'-') 'Digitador2',ifnull(A.observac_llamada2,'-') 'Observaciones de Inasistencia',FN_CATALOGODESC(276,A.estado) 'Estado'
+	ifnull(A.usu_update,'-') 'Digitador1',ifnull(A.observac_llamadas,'-') 'Observaciones de Recordación',ifnull(A.fecha_llamada2,'-') 'Fecha Llamada por Efectividad',ifnull(A.nombre_llamada2,'-') 'Nombre quien Contesto Llamada',ifnull(FN_CATALOGODESC(282,A.motivo_inasistencia),'-') 'Motivo de la Inasistencia',ifnull(A.reasigno,'-') 'Se reasigno la Cita',ifnull(A.usu_update,'-') 'Digitador2',ifnull(A.observac_llamada2,'-') 'Observaciones de Inasistencia',FN_CATALOGODESC(276,A.estado) 'Estado'
 FROM agendamiento A 
 	LEFT JOIN person P ON A.idpeople = P.idpeople
 	LEFT JOIN usuarios U ON A.usu_creo = U.id_usuario
